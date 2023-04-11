3558e1df917df559d10d132a209fda6630231970
PedroHLC ☭ <root@pedrohlc.com>
2023-04-11 13:46:11 -0300

    builder: lazy push; some overridable variables; success/failures/errors logs

--- shared/builder.nix
+++ shared/builder.nix
@@ -1,3 +1,4 @@
+# The smallest and KISSer continuos-deploy I was able to create.
 { all-packages
 , cachix
 , jq
@@ -5,19 +6,18 @@
 , nix
 , flakeSelf
 , writeShellScriptBin
+, writeShellScript
 }:
 let
   evalCommand = where: drvOutputs:
     let
-      derivation = "${flakeSelf}#${where}";
-      outputs = map (guide derivation) drvOutputs;
+      derivation = "$NYX_SOURCE#${where}";
+      fullTag = output: "\"${guide derivation output}\"";
+      outputs = map fullTag drvOutputs;
     in
     ''
-      echo "Building ${derivation}"
-      ${nix}/bin/nix build --json \
-        ${lib.strings.concatStringsSep " \\\n  " outputs} |\
-        ${jq}/bin/jq -r '.[].outputs[]' |\
-        ${cachix}/bin/cachix push chaotic-nyx --compression-method zstd
+      build "${where}" \
+        ${lib.strings.concatStringsSep " \\\n  " outputs}
     '';
 
   guide = namespace: n:
@@ -48,7 +48,50 @@ let
   ;
 in
 writeShellScriptBin "build-chaotic-nyx" ''
-  cd "$(mktemp -d)"
+  NYX_SOURCE="''${NYX_SOURCE:-${flakeSelf}}"
+  NYX_FLAGS="''${NYX_FLAGS:---accept-flake-config}"
+  NYX_WD="''${NYX_WD:-$(mktemp -d)}"
+  R='\033[0;31m'
+  G='\033[0;32m'
+  W='\033[0m'
+
+  cd "$NYX_WD"
+  echo -n "" > push.txt > errors.txt > success.txt > failures.txt
+
+  function build() {
+    _WHAT="''${1:- アンノーン}"
+    echo -n "Building $_WHAT..."
+    if \
+      ( set -o pipefail;
+        ${nix}/bin/nix build --json $NYX_FLAGS "''${@:2}" |\
+          ${jq}/bin/jq -r '.[].outputs[]' \
+      ) 2>> errors.txt >> push.txt
+    then
+      echo "$_WHAT" >> success.txt
+      echo -e "''${G} OK''${W}"
+    else
+      echo "$_WHAT" >> failures.txt
+      echo -e "''${R} ERR''${W}"
+    fi
+  }
 
   ${packagesEval "" "" all-packages}
+
+  if [ -z "$CACHIX_AUTH_TOKEN" ] && [ -z "$CACHIX_SIGNING_KEY" ]; then
+    echo "No key for cachix -- ignoring deploy."
+    exit 23
+  elif [ -s push.txt ]; then
+    # Let nix digest store paths first
+    sleep 10
+
+    echo "Pushing to cache..."
+    cat push.txt | ${cachix}/bin/cachix push chaotic-nyx \
+      --compression-method zstd
+
+  else
+    echo "Nothing to push."
+    exit 42
+  fi
+
+  exit 0
 ''
