856c04d92498cac80fb22f2b7832dc7c98068c8b
Nico Jensch <root@dr460nf1r3.org>
2023-04-12 22:02:42 +0200

    Another try of fixing the config

--- pkgs/linux-cachyos/default.nix
+++ pkgs/linux-cachyos/default.nix
@@ -26,71 +26,75 @@ let
   };
 in
 
-(pkgs.linux_6_2.override { argsOverride = rec {
-  version = "${major}.${minor}";
+(pkgs.linux_6_2.override {
+  argsOverride = rec {
+    version = "${major}.${minor}";
 
-  src = fetchurl {
-    url = "mirror://kernel/linux/kernel/v6.x/linux-${major}.${minor}.tar.xz";
-    sha256 = "sha256-V8Viw80nU/IyVJyrBcitdw7YSK6GQBYZx1gb3/rupP4=";
+    src = fetchurl {
+      url = "mirror://kernel/linux/kernel/v6.x/linux-${major}.${minor}.tar.xz";
+      sha256 = "sha256-V8Viw80nU/IyVJyrBcitdw7YSK6GQBYZx1gb3/rupP4=";
+    };
+
+    extraMeta = { maintainers = with lib; [ maintainers.dr460nf1r3 ]; };
+
+    configfile = "${config-src}/linux-cachyos/config";
+
+    allowImportFromDerivation = true;
+
+    kernelPatches =
+      builtins.map
+        (name: {
+          inherit name;
+          patch = name;
+        })
+        [
+          "${patches-src}/${major}/all/0001-cachyos-base-all.patch"
+          "${patches-src}/${major}/misc/0001-Add-latency-priority-for-CFS-class.patch"
+          "${patches-src}/${major}/sched/0001-bore-cachy.patch"
+        ];
+
+    structuredExtraConfig = with lib.kernel; {
+      EXPERT = no;
+      WERROR = no;
+
+      # Tick to 500hz
+      HZ = freeform "500";
+      HZ_500 = yes;
+      HZ_1000 = no;
+
+      # Disable MQ Deadline I/O scheduler
+      MQ_IOSCHED_DEADLINE = lib.mkForce no;
+
+      # Disable Kyber I/O scheduler
+      MQ_IOSCHED_KYBER = lib.mkForce no;
+
+      # Enabling full ticks
+      CONTEXT_TRACKING_FORCE = option no;
+      HZ_PERIODIC = no;
+      NO_HZ_FULL_NODEF = option yes;
+      NO_HZ_IDLE = no;
+
+      # Enable O3
+      CC_OPTIMIZE_FOR_PERFORMANCE = no;
+      CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
+
+      # Enable bbr2
+      DEFAULT_BBR2 = yes;
+      DEFAULT_CUBIC = option no;
+      DEFAULT_TCP_CONG = freeform "bbr2";
+      TCP_CONG_BBR2 = yes;
+      TCP_CONG_CUBIC = lib.mkForce module;
+
+      # Enable zram/zswap ZSTD compression
+      MODULE_COMPRESS_ZSTD_LEVEL = option (freeform "9");
+      MODULE_COMPRESS_ZSTD_ULTRA = option no;
+      ZRAM_DEF_COMP = freeform "zstd";
+      ZRAM_DEF_COMP_LZORLE = no;
+      ZRAM_DEF_COMP_ZSTD = yes;
+      ZSTD_COMPRESSION_LEVEL = freeform "19";
+      ZSWAP_COMPRESSOR_DEFAULT = freeform "zstd";
+      ZSWAP_COMPRESSOR_DEFAULT_LZO = no;
+      ZSWAP_COMPRESSOR_DEFAULT_ZSTD = yes;
+    };
   };
-
-  extraMeta = { maintainers = with lib; [ maintainers.dr460nf1r3 ]; };
-
-  configfile = "${config-src}/linux-cachyos/config";
-
-  kernelPatches =
-    builtins.map
-      (name: {
-        inherit name;
-        patch = name;
-      })
-      [
-        "${patches-src}/${major}/all/0001-cachyos-base-all.patch"
-        "${patches-src}/${major}/misc/0001-Add-latency-priority-for-CFS-class.patch"
-        "${patches-src}/${major}/sched/0001-bore-cachy.patch"
-      ];
-
-  structuredExtraConfig = with lib.kernel; {
-    EXPERT = no;
-    WERROR = no;
-
-    # Tick to 500hz
-    HZ = freeform "500";
-    HZ_500 = yes;
-    HZ_1000 = no;
-
-    # Disable MQ Deadline I/O scheduler
-    MQ_IOSCHED_DEADLINE = lib.mkForce no;
-
-    # Disable Kyber I/O scheduler
-    MQ_IOSCHED_KYBER = lib.mkForce no;
-
-    # Enabling full ticks
-    CONTEXT_TRACKING_FORCE = option no;
-    HZ_PERIODIC = no;
-    NO_HZ_FULL_NODEF = option yes;
-    NO_HZ_IDLE = no;
-
-    # Enable O3
-    CC_OPTIMIZE_FOR_PERFORMANCE = no;
-    CC_OPTIMIZE_FOR_PERFORMANCE_O3 = yes;
-
-    # Enable bbr2
-    DEFAULT_BBR2 = yes;
-    DEFAULT_CUBIC = option no;
-    DEFAULT_TCP_CONG = freeform "bbr2";
-    TCP_CONG_BBR2 = yes;
-    TCP_CONG_CUBIC = lib.mkForce module;
-
-    # Enable zram/zswap ZSTD compression
-    MODULE_COMPRESS_ZSTD_LEVEL = option (freeform "9");
-    MODULE_COMPRESS_ZSTD_ULTRA = option no;
-    ZRAM_DEF_COMP = freeform "zstd";
-    ZRAM_DEF_COMP_LZORLE = no;
-    ZRAM_DEF_COMP_ZSTD = yes;
-    ZSTD_COMPRESSION_LEVEL = freeform "19";
-    ZSWAP_COMPRESSOR_DEFAULT = freeform "zstd";
-    ZSWAP_COMPRESSOR_DEFAULT_LZO = no;
-    ZSWAP_COMPRESSOR_DEFAULT_ZSTD = yes;
-  };};
 })
