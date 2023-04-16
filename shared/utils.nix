cb2eefc077d69b4da18e4f91175913221112c40b
PedroHLC ☭ <root@pedrohlc.com>
2023-04-16 10:37:37 -0300

    nyxUtils: promote

--- shared/utils.nix
+++ shared/utils.nix
@@ -1,4 +1,12 @@
 { lib }:
-{
+rec {
   dropN = n: list: lib.lists.take (builtins.length list - n) list;
+
+  gitToVersion = src: "unstable-${src.lastModifiedDate}-${src.shortRev}";
+
+  gitOverride = src: drv:
+    drv.overrideAttrs (_: {
+      version = gitToVersion src;
+      inherit src;
+    });
 }
