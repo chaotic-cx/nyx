4a5043803cd5109fd3eb78258317c84297ee0ad3
Nico Jensch <root@dr460nf1r3.org>
2023-04-21 19:01:55 +0200

    module: appmenu-gtk3-module.enable fix & add package

--- modules/appmenu-gtk3-module.nix
+++ modules/appmenu-gtk3-module.nix
@@ -14,7 +14,9 @@ in
   };
   config = {
     environment.profileRelativeSessionVariables = lib.mkIf cfg.enable {
-      XDG_DATA_DIRS = [ "${pkgs.appmenu-gtk3-module}/share/gsettings-schemas/appmenu-gtk3-module-0.7.6" ];
+      XDG_DATA_DIRS = [ "/share/gsettings-schemas/appmenu-gtk3-module-0.7.6" ];
     };
+
+    environment.systemPackages = with pkgs; [ appmenu-gtk3-module ];
   };
 }
