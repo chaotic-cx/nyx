446a30464bdfc003de5c9faa12aae42359d8e68b
Nico Jensch <root@dr460nf1r3.org>
2023-04-25 20:30:46 +0200

    appmenu-gtk3-module: use sessionVariables

--- modules/appmenu-gtk3-module.nix
+++ modules/appmenu-gtk3-module.nix
@@ -13,12 +13,12 @@ in
       };
   };
   config = {
-    environment.profileRelativeSessionVariables = lib.mkIf cfg.enable {
-      XDG_DATA_DIRS = [ "/share/gsettings-schemas/appmenu-gtk3-module-0.7.6" ];
+    environment.sessionVariables = lib.mkIf cfg.enable {
+      XDG_DATA_DIRS = [ "${pkgs.appmenu-gtk3-module}/share/gsettings-schemas/appmenu-gtk3-module-0.7.6" ];
     };
 
     environment.systemPackages = with pkgs; [ appmenu-gtk3-module ];
 
-    environment.variables = { "UBUNTU_MENUPROXY" = "1"; };
+    environment.variables."UBUNTU_MENUPROXY" = "1";
   };
 }
