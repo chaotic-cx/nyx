8933701c2b4f620c5c79972b20585452d7ad46f4
PedroHLC ☭ <root@pedrohlc.com>
2023-05-04 17:18:01 -0300

    linux_cachyos: add note about nixpkgs#216529 (fmt)

--- pkgs/linux-cachyos/default.nix
+++ pkgs/linux-cachyos/default.nix
@@ -53,7 +53,8 @@ in
 
   extraMeta = { maintainers = with lib; [ maintainers.dr460nf1r3 ]; };
 }
-).overrideAttrs (pa: { # bypasses https://github.com/NixOS/nixpkgs/issues/216529
+).overrideAttrs (pa: {
+  # bypasses https://github.com/NixOS/nixpkgs/issues/216529
   passthru = pa.passthru // {
     features = {
       efiBootStub = true;
