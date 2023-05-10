6b554ab8f9207e2468308e1ce89f504e853c460b
PedroHLC ☭ <root@pedrohlc.com>
2023-05-10 11:28:04 -0300

    gamescope_git: update buildInputs

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -4,5 +4,5 @@ prev.gamescope.overrideAttrs (pa: {
   version = nyxUtils.gitToVersion inputs.gamescope-git-src;
   src = inputs.gamescope-git-src;
   patches = [ (final.lib.lists.take 1 pa.patches) ];
-  buildInputs = pa.buildInputs ++ (with final; [ glm ]);
+  buildInputs = pa.buildInputs ++ (with final; [ glm gbenchmark ]);
 })
