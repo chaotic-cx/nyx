01d77eb027c5f474be946bed7611b270f83d54a3
PedroHLC ☭ <root@pedrohlc.com>
2023-05-13 17:11:11 -0300

    linuxPackages_cachyos.zfsUnstable: use cachyos' revision (7)

--- overlays/default.nix
+++ overlays/default.nix
@@ -90,7 +90,23 @@ in
     ];
   };
 
-  linuxPackages_cachyos = final.linuxPackagesFor final.linux_cachyos;
+  linuxPackages_cachyos =
+    let
+      base = final.linuxPackagesFor final.linux_cachyos;
+
+      zfs = base.zfsUnstable.overrideAttrs (pa: {
+        src =
+          final.fetchFromGitHub {
+            owner = "cachyos";
+            repo = "zfs";
+            rev = "ac18dc77f3703940682aecb442f4e58aa2c14f1a";
+            hash = "sha256-wrMZYENs4hmrHXcSN4kYgntaDDs5IwOMeWWqUKortbw=";
+          };
+        meta = pa.meta // { broken = false; };
+        patches = [ ];
+      });
+    in
+    base // { zfs = zfs; zfsStable = zfs; zfsUnstable = zfs; };
 
   linuxPackages_hdr = final.linuxPackagesFor final.linux_hdr;
 
@@ -149,16 +165,4 @@ in
   };
 
   yuzu-early-access_git = callOverride ../pkgs/yuzu-ea-git { };
-
-  zfs_cachyos = final.linuxPackages_cachyos.zfsUnstable.overrideAttrs (pa: {
-    src =
-      final.fetchFromGitHub {
-        owner = "cachyos";
-        repo = "zfs";
-        rev = "ac18dc77f3703940682aecb442f4e58aa2c14f1a";
-        hash = "sha256-wrMZYENs4hmrHXcSN4kYgntaDDs5IwOMeWWqUKortbw=";
-      };
-    meta = pa.meta // { broken = false; };
-    patches = [];
-  });
 }
