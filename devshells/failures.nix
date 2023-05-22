1b3f0f0cc89baeb5478c12aa26815e2c752514fe
PedroHLC ☭ <root@pedrohlc.com>
2023-05-22 14:10:32 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,9 +1,8 @@
 {
   "linuxPackages_cachyos.apfs" = "/nix/store/60781lcjza51x6nfbrh5l60x99d17hn0-apfs-0.3.1-6.3.3";
-  "linuxPackages_cachyos.cryptodev" = "/nix/store/a56kszr3hc5n1wp3am6m7ahpx3p6whl8-cryptodev-linux-1.12-6.3.3";
   "linuxPackages_cachyos.ddcci-driver" = "/nix/store/zd0pmx8dbvn5ymp5gmzva2xixw3g4ich-ddcci-driver-6.3.3-0.4.3";
   "linuxPackages_cachyos.digimend" = "/nix/store/ipl87nvwxfpmr9va2whwjrijzv1gc20f-digimend-10";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/lzhrs7yq96cg20qbpf5srvara6rmq6ki-dpdk-22.11.1-6.3.3";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/vvhinz1xs1gqr0idxg6kfy88clqrxa2y-dpdk-22.11.1-6.3.3";
   "linuxPackages_cachyos.ena" = "/nix/store/w6r7llp3jg46qx4k9hgh7piz50b3vakh-ena-2.8.3-6.3.3";
   "linuxPackages_cachyos.evdi" = "/nix/store/4y0r0anllbpnw0xizazv0sqmn0ij1cq0-evdi-unstable-2022-10-13";
   "linuxPackages_cachyos.isgx" = "/nix/store/6xdn2jij9hnwan23ik52ggrghw83jyhg-isgx-2.14-6.3.3";
@@ -11,7 +10,6 @@
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/mz2dqbx2kxvwnpw8pvhnnyj9z20ym33w-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_cachyos.qc71_laptop" = "/nix/store/1jq93n3xkj8r5pj2lr24zglf1kxzxl67-qc71_laptop-unstable-2022-06-01";
   "linuxPackages_cachyos.rtl8189fs" = "/nix/store/h70zrz4vjnk57hpfngh0fp5sbh0kwsg5-rtl8189fs-6.3.3-2022-10-30";
-  "linuxPackages_cachyos.rtl8812au" = "/nix/store/3mya14fxlc6z9hsvd6szgzx4kxsnmzd6-rtl8812au-6.3.3-unstable-2023-01-17";
   "linuxPackages_cachyos.rtl8814au" = "/nix/store/7ha15w1z5p8w4sxq37aj916ivmchd07p-rtl8814au-6.3.3-unstable-2022-11-09";
   "linuxPackages_cachyos.rtl8821au" = "/nix/store/mgd200lkk8bxipcddqy836f64425fi5g-rtl8821au-6.3.3-unstable-2022-12-22";
   "linuxPackages_cachyos.rtl8821ce" = "/nix/store/jaanaahnd4b43f85vf18av7bcbapg5p0-rtl8821ce-6.3.3-unstable-2023-01-01";
