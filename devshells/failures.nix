6c79d68daefcce6d14d8a4e17eaea5e61b3581c3
Pedro Lara Campos <root@pedrohlc.com>
2023-05-23 17:18:16 +0000

    flake-20230523-1: gamescope, mesa, wlroots, yuzu-ea (#123)

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,6 +1,5 @@
 {
   "linuxPackages_cachyos.apfs" = "/nix/store/60781lcjza51x6nfbrh5l60x99d17hn0-apfs-0.3.1-6.3.3";
-  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/zd0pmx8dbvn5ymp5gmzva2xixw3g4ich-ddcci-driver-6.3.3-0.4.3";
   "linuxPackages_cachyos.digimend" = "/nix/store/ipl87nvwxfpmr9va2whwjrijzv1gc20f-digimend-10";
   "linuxPackages_cachyos.dpdk" = "/nix/store/vvhinz1xs1gqr0idxg6kfy88clqrxa2y-dpdk-22.11.1-6.3.3";
   "linuxPackages_cachyos.ena" = "/nix/store/w6r7llp3jg46qx4k9hgh7piz50b3vakh-ena-2.8.3-6.3.3";
@@ -10,16 +9,11 @@
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/mz2dqbx2kxvwnpw8pvhnnyj9z20ym33w-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_cachyos.qc71_laptop" = "/nix/store/1jq93n3xkj8r5pj2lr24zglf1kxzxl67-qc71_laptop-unstable-2022-06-01";
   "linuxPackages_cachyos.rtl8189fs" = "/nix/store/h70zrz4vjnk57hpfngh0fp5sbh0kwsg5-rtl8189fs-6.3.3-2022-10-30";
-  "linuxPackages_cachyos.rtl8814au" = "/nix/store/7ha15w1z5p8w4sxq37aj916ivmchd07p-rtl8814au-6.3.3-unstable-2022-11-09";
-  "linuxPackages_cachyos.rtl8821au" = "/nix/store/mgd200lkk8bxipcddqy836f64425fi5g-rtl8821au-6.3.3-unstable-2022-12-22";
-  "linuxPackages_cachyos.rtl8821ce" = "/nix/store/jaanaahnd4b43f85vf18av7bcbapg5p0-rtl8821ce-6.3.3-unstable-2023-01-01";
   "linuxPackages_cachyos.rtl8821cu" = "/nix/store/smz3akrp2z8g8cjal2r3cnnzgwwjrrvq-rtl8821cu-6.3.3-unstable-2022-12-07";
-  "linuxPackages_cachyos.rtl88xxau-aircrack" = "/nix/store/fa6r6hvl8049b8qzzz0nn1hx5plfrfdx-rtl88xxau-aircrack-6.3.3-ee2997";
   "linuxPackages_cachyos.sysdig" = "/nix/store/zf5xpkcw83734wcr3484ivp39hfqxmwh-sysdig-0.31.5";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/mkz4lj0sm90zkl9pay2q227vq61051sr-system76-acpi-module-1.0.2-6.3.3";
   "linuxPackages_cachyos.virtualbox" = "/nix/store/gjmi94npbish1fv37986r38vyc5rfvwx-virtualbox-modules-7.0.6-6.3.3";
   "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/mz66j07rhmxql85apigdam8qjnrlchgp-VirtualBox-GuestAdditions-7.0.6-6.3.3";
-  "linuxPackages_cachyos.xone" = "/nix/store/h39ljvj7pjf94y1wll1l5nxi7wx4w0qj-xone-0.3";
   "linuxPackages_hdr.mba6x_bl" = "/nix/store/rg4yl1w4dmjmkd48bryjpcg81gz9gmws-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/l1gb661xs2ykkqrc3n4ci4p76ba9038m-VirtualBox-GuestAdditions-7.0.6-6.0-unstable-2023-01-17";
 }
