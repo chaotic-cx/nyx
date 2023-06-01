cb99cc5949a37979d575fbd4bc52bb0825012d56
Pedro Lara Campos <root@pedrohlc.com>
2023-06-01 12:18:01 +0000

    flake-20230601-1 + linux_cachyos (#135)
    Daily bump of our precious packages.

--- pkgs/linux-cachyos/versions.nix
+++ pkgs/linux-cachyos/versions.nix
@@ -3,16 +3,16 @@
   suffix = "-cachyos";
 
   # pkgver from config's PKGBUILD
-  linux.version = "6.3.4";
-  linux.hash = "sha256-2GJ1KO1rOuYH0Ase9aRuDnBRrkCyhf1OgvT/C7craOg=";
+  linux.version = "6.3.5";
+  linux.hash = "sha256-9c1HjD2LkIq2Bq/R6VpPj3fnGGtKgoKSUdbmqq//gl4=";
 
-  # latest commit from https://github.com/CachyOS/linux-cachyos/commits/master
-  config.rev = "2eedd656d8bbef6fe7deac8d1daf2af685553615";
-  config.hash = "sha256-22pBAYR1EqAj1tAgAmVK6i2hWgYbvSD/cvYwS0v7FAU=";
+  # latest commit from https://github.com/CachyOS/linux-cachyos/commits/master/linux-cachyos
+  config.rev = "e64ec43be2ab67972fb84e942c3be52c9c78f5dd";
+  config.hash = "sha256-q92B67rF82us4fnVxyck7KgriQwNVyDIcVIZnXDLB5I=";
 
   # latest commit from https://github.com/CachyOS/kernel-patches/commits/master/6.3
-  patches.rev = "1f326f7983982f0c23f46cc14368363e3bcfbd8c";
-  patches.hash = "sha256-H2LDC5zQhMvPIY/jOV7aN7OW16yMSVgHf1XsOGdzF2E=";
+  patches.rev = "cf0d7cecefc55a8a9332927974b863462731e4fa";
+  patches.hash = "sha256-xbWe8NKV/h96GHUsnimdaPmaF1TBHSmw4evcWVUmMB0=";
 
   # search for git+https://github.com/cachyos/zfs.git in config's PKGBUILD
   zfs.rev = "ac18dc77f3703940682aecb442f4e58aa2c14f1a";
