5a9a42200338a093fcc1aa366395cac78fcca0f5
PedroHLC ☭ <root@pedrohlc.com>
2023-06-01 09:09:23 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,15 +1,15 @@
 {
-  "linuxPackages_cachyos.apfs" = "/nix/store/j33zfm89z2213khjij4fg6a85vbx6hyi-apfs-0.3.1-6.3.4";
-  "linuxPackages_cachyos.digimend" = "/nix/store/8vz0ppa0k9iwab3l55hchz1kckwdq7km-digimend-10";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/r956znp3rdic7k2nmvx0n39n5c2pc1zw-dpdk-22.11.1-6.3.4";
-  "linuxPackages_cachyos.evdi" = "/nix/store/nyqhwsnfg2l5hwrdlnyin51pakinvrqc-evdi-unstable-2022-10-13";
-  "linuxPackages_cachyos.isgx" = "/nix/store/5z1v6gbg5m6i3am5akz8h761vkwrdsx6-isgx-2.14-6.3.4";
-  "linuxPackages_cachyos.lttng-modules" = "/nix/store/n2xm0jy31h03hz90znr41mq7snp3wyvf-lttng-modules-6.3.4-2.13.8";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/xvparavrf6r2zccb1vas1n4lrv7jxzrq-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_cachyos.sysdig" = "/nix/store/2y1frayfkrb6zxxabxb80b99gy5g6yqp-sysdig-0.31.5";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/cyr16ka3cl6kxq24phm4ykx57k4k5y50-system76-acpi-module-1.0.2-6.3.4";
-  "linuxPackages_cachyos.virtualbox" = "/nix/store/lw61a3psqm2n53ys3c44rz95rjd3mw8b-virtualbox-modules-7.0.6-6.3.4";
-  "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/47kppk8br9f1p60jmvla15im1yyl04n9-VirtualBox-GuestAdditions-7.0.6-6.3.4";
+  "linuxPackages_cachyos.apfs" = "/nix/store/dx29l727j0jw927ai6l7rxgc8af507iw-apfs-0.3.1-6.3.5";
+  "linuxPackages_cachyos.digimend" = "/nix/store/h6x76hdyiv6qnz1n1q5vqnz89dgav86s-digimend-10";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/2pwd9cp6w10nmya820asf0lbdhz8kj1w-dpdk-22.11.1-6.3.5";
+  "linuxPackages_cachyos.evdi" = "/nix/store/wfgfxdfdjdra7lj4r5bf3dwrr54swr1c-evdi-unstable-2022-10-13";
+  "linuxPackages_cachyos.isgx" = "/nix/store/ri1qpp1ax1zbpj66l8158dp6jslss24p-isgx-2.14-6.3.5";
+  "linuxPackages_cachyos.lttng-modules" = "/nix/store/5pq4jp0gk33li4aq7216agdqs2hnca18-lttng-modules-6.3.5-2.13.8";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/fvxbj16c72kj58f81z5d4cgrmz0aishd-mba6x_bl-unstable-2016-12-08";
+  "linuxPackages_cachyos.sysdig" = "/nix/store/va6nkajkhvgrskavmi4vyw61hrxyym4c-sysdig-0.31.5";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/8bjc01c9b2ddahzhmfqp0dy8ji4bjyqf-system76-acpi-module-1.0.2-6.3.5";
+  "linuxPackages_cachyos.virtualbox" = "/nix/store/50r0pq0vsbhh0mmr1fzsjj5m9hlwsxf3-virtualbox-modules-7.0.6-6.3.5";
+  "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/nxid0lcrx6bxnx7n72w438n5vxwj90zz-VirtualBox-GuestAdditions-7.0.6-6.3.5";
   "linuxPackages_hdr.mba6x_bl" = "/nix/store/rg4yl1w4dmjmkd48bryjpcg81gz9gmws-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/l1gb661xs2ykkqrc3n4ci4p76ba9038m-VirtualBox-GuestAdditions-7.0.6-6.0-unstable-2023-01-17";
 }
