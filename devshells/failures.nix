5f6753396813090d4eb45972d57352002055c285
Pedro Lara Campos <root@pedrohlc.com>
2023-06-12 19:00:52 +0000

    flake-20230612-1: input-leap, mangohud, mesa, sway, wlroots, yuzu-ea (#162)
    - Daily bump of our precious packages;
    - expose linux_cachyos' configfile so users won't have to build it.

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -5,7 +5,6 @@
   "linuxPackages_cachyos.isgx" = "/nix/store/nxw3cyrlcjq74sblcyx2rmcg9dff8sv5-isgx-2.14-6.3.7";
   "linuxPackages_cachyos.lttng-modules" = "/nix/store/9c6pqq7lwqpqif6xa1sa4mshiylqwry1-lttng-modules-6.3.7-2.13.8";
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/4c7cypqpr6bqbbvsg2b2n0ww3mb24hjk-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_cachyos.sysdig" = "/nix/store/c5f66pqgw4vjxg6va4pq3mlxw5zixp75-sysdig-0.31.5";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/svvg604j0vxd0xb9d7w6nby8drwvdri2-system76-acpi-module-1.0.2-6.3.7";
   "linuxPackages_hdr.mba6x_bl" = "/nix/store/wbbgz76ndmymp8m2nc6w5xf7kh7m0fd7-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/c6gd18fvk9qfs9gigxavkx1ziy4kyqmi-VirtualBox-GuestAdditions-7.0.8-6.0-unstable-2023-01-17";
