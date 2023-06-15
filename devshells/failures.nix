64221e71d83a5c0aacd10a12ee600769676defaf
Nico <github@dr460nf1r3.org>
2023-06-15 18:27:08 +0200

    linux-cachyos: update 6.3.8 (#164)
    ### :fish: What?
    
    Updated `linux_cachyos`; 
    
    ### :fishing_pole_and_fish: Why?
    
    Get the latest version;
    
    ---------
    
    Co-authored-by: PedroHLC ☭ <root@pedrohlc.com>

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,11 +1,11 @@
 {
-  "linuxPackages_cachyos.digimend" = "/nix/store/rj3hkfzh0lzplw752b235sabg33admlk-digimend-10";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/s5dfwiyx4i1jg99v4ydpb4l3k9rnwivr-dpdk-22.11.1-6.3.7";
-  "linuxPackages_cachyos.evdi" = "/nix/store/5l0x1whsvirr7yj942cyslvpcvgcpv1w-evdi-unstable-2022-10-13";
-  "linuxPackages_cachyos.isgx" = "/nix/store/nxw3cyrlcjq74sblcyx2rmcg9dff8sv5-isgx-2.14-6.3.7";
-  "linuxPackages_cachyos.lttng-modules" = "/nix/store/9c6pqq7lwqpqif6xa1sa4mshiylqwry1-lttng-modules-6.3.7-2.13.8";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/4c7cypqpr6bqbbvsg2b2n0ww3mb24hjk-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/svvg604j0vxd0xb9d7w6nby8drwvdri2-system76-acpi-module-1.0.2-6.3.7";
+  "linuxPackages_cachyos.digimend" = "/nix/store/mv6fk8ganlr98n81b5573w76fg4b2pad-digimend-10";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/cn2rrcz7hi8kjh95q73mkdr2cvy466ai-dpdk-22.11.1-6.3.8";
+  "linuxPackages_cachyos.evdi" = "/nix/store/br87nphnvn1a2qgd3ps0wnrn02fyq54s-evdi-unstable-2022-10-13";
+  "linuxPackages_cachyos.isgx" = "/nix/store/p0rwzvfxfgg1gr36789dfjvqmgixyjlh-isgx-2.14-6.3.8";
+  "linuxPackages_cachyos.lttng-modules" = "/nix/store/irhpb7mjypadf5wlm81rgb67n28bbdnl-lttng-modules-6.3.8-2.13.8";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/88bxrj6zldwcv0pbigcr3mfxb9xx0zh6-mba6x_bl-unstable-2016-12-08";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/2p1paql2qz0n3qm683rbfjiqhyawzl5h-system76-acpi-module-1.0.2-6.3.8";
   "linuxPackages_hdr.mba6x_bl" = "/nix/store/wbbgz76ndmymp8m2nc6w5xf7kh7m0fd7-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/c6gd18fvk9qfs9gigxavkx1ziy4kyqmi-VirtualBox-GuestAdditions-7.0.8-6.0-unstable-2023-01-17";
 }
