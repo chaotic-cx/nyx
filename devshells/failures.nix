1c007627372de67c4136a6b2c0eeeeeb5bb27ea6
PedroHLC ☭ <root@pedrohlc.com>
2023-06-23 10:09:36 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,11 +1,10 @@
 {
-  "linuxPackages_cachyos.digimend" = "/nix/store/mv6fk8ganlr98n81b5573w76fg4b2pad-digimend-10";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/cn2rrcz7hi8kjh95q73mkdr2cvy466ai-dpdk-22.11.1-6.3.8";
-  "linuxPackages_cachyos.evdi" = "/nix/store/br87nphnvn1a2qgd3ps0wnrn02fyq54s-evdi-unstable-2022-10-13";
-  "linuxPackages_cachyos.isgx" = "/nix/store/p0rwzvfxfgg1gr36789dfjvqmgixyjlh-isgx-2.14-6.3.8";
-  "linuxPackages_cachyos.lttng-modules" = "/nix/store/irhpb7mjypadf5wlm81rgb67n28bbdnl-lttng-modules-6.3.8-2.13.8";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/88bxrj6zldwcv0pbigcr3mfxb9xx0zh6-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/2p1paql2qz0n3qm683rbfjiqhyawzl5h-system76-acpi-module-1.0.2-6.3.8";
-  "linuxPackages_hdr.mba6x_bl" = "/nix/store/wbbgz76ndmymp8m2nc6w5xf7kh7m0fd7-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/c6gd18fvk9qfs9gigxavkx1ziy4kyqmi-VirtualBox-GuestAdditions-7.0.8-6.0-unstable-2023-01-17";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/nvrnwy17hs6h9kfj629kazry74lgb10s-dpdk-22.11.1-6.3.8";
+  "linuxPackages_cachyos.evdi" = "/nix/store/ggnf10ymff0jci234x5axjwc4qxqi9vw-evdi-unstable-2022-10-13";
+  "linuxPackages_cachyos.isgx" = "/nix/store/v5wk40pp50ddp4c5wm1bi3jbrp65kdfq-isgx-2.14-6.3.8";
+  "linuxPackages_cachyos.lttng-modules" = "/nix/store/10ah2s6nimjysp4145gxv346f5imw344-lttng-modules-6.3.8-2.13.8";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/1ayxnlmnp4iibcw4m7ajkq52y5z6scjw-mba6x_bl-unstable-2016-12-08";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/ibhisq3d9qcsrzqfgw32plf2bymnj3p4-system76-acpi-module-1.0.2-6.3.8";
+  "linuxPackages_hdr.mba6x_bl" = "/nix/store/5q76s0sh5aim887x6bgnjkzzvm5jz00m-mba6x_bl-unstable-2016-12-08";
+  "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/gy6k6n758941qws0can16v26lqbh0lnp-VirtualBox-GuestAdditions-7.0.8-6.0-unstable-2023-01-17";
 }
