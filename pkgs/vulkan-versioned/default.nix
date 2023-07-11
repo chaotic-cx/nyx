7e1e0ec5bd29f29ec0b1657c09162276a7c5cf38
PedroHLC ☭ <root@pedrohlc.com>
2023-07-11 08:46:30 -0300

    vulkanPackages_latest: remove broken as WIP

--- pkgs/vulkan-versioned/default.nix
+++ pkgs/vulkan-versioned/default.nix
@@ -7,18 +7,18 @@
 {
   vulkanVersions = self.dontRecurseIntoAttrs vulkanVersions;
 
-  gfxreconstruct = (prev.gfxreconstruct.override
-    { inherit (self) vulkan-loader; }).overrideAttrs (pa: with vulkanVersions; {
-    nativeBuildInputs = pa.nativeBuildInputs ++ [ self.vulkan-headers ];
-    version = gfxreconstructVersion;
-    src = final.fetchFromGitHub {
-      owner = "LunarG";
-      repo = "gfxreconstruct";
-      rev = gfxreconstructRev;
-      hash = gfxreconstructHash;
-    };
-    meta = pa.meta // { broken = gfxreconstructVersion == "1.0.0"; };
-  });
+  #gfxreconstruct = (prev.gfxreconstruct.override
+  #  { inherit (self) vulkan-loader; }).overrideAttrs (pa: with vulkanVersions; {
+  #  nativeBuildInputs = pa.nativeBuildInputs ++ [ self.vulkan-headers ];
+  #  version = gfxreconstructVersion;
+  #  src = final.fetchFromGitHub {
+  #    owner = "LunarG";
+  #    repo = "gfxreconstruct";
+  #    rev = gfxreconstructRev;
+  #    hash = gfxreconstructHash;
+  #  };
+  #  meta = pa.meta // { broken = gfxreconstructVersion == "1.0.0"; };
+  #});
 
   glslang = (prev.glslang.override
     { inherit (self) spirv-headers spirv-tools; }).overrideAttrs (pa: with vulkanVersions; {
@@ -103,27 +103,27 @@
       hash = vulkanToolsHash;
     };
   });
-  vulkan-tools-lunarg = (prev.vulkan-tools-lunarg.override
-    { inherit (self) vulkan-headers vulkan-loader vulkan-validation-layers; }).overrideAttrs (pa: with vulkanVersions; {
-    nativeBuildInputs = pa.nativeBuildInputs ++ [ final.xorg.libXau ];
-    version = vulkanToolsLunarGVersion;
-    src = final.fetchFromGitHub {
-      owner = "LunarG";
-      repo = "VulkanTools";
-      rev = vulkanToolsLunarGRev;
-      hash = vulkanToolsLunarGHash;
-      fetchSubmodules = true;
-    };
-  });
-  vulkan-validation-layers = (prev.vulkan-validation-layers.override
-    { inherit (self) vulkan-headers spirv-headers; }).overrideAttrs (pa: with vulkanVersions; {
-    version = vulkanValidationLayersVersion;
-    src = final.fetchFromGitHub {
-      owner = "KhronosGroup";
-      repo = "Vulkan-ValidationLayers";
-      rev = vulkanValidationLayersRev;
-      hash = vulkanValidationLayersHash;
-    };
-    meta = pa.meta // { broken = vulkanValidationLayersVersion == "1.3.256" && vulkanHeadersVersion == "1.3.257"; };
-  });
+  #vulkan-tools-lunarg = (prev.vulkan-tools-lunarg.override
+  #  { inherit (self) vulkan-headers vulkan-loader vulkan-validation-layers; }).overrideAttrs (pa: with vulkanVersions; {
+  #  nativeBuildInputs = pa.nativeBuildInputs ++ [ final.xorg.libXau ];
+  #  version = vulkanToolsLunarGVersion;
+  #  src = final.fetchFromGitHub {
+  #    owner = "LunarG";
+  #    repo = "VulkanTools";
+  #    rev = vulkanToolsLunarGRev;
+  #    hash = vulkanToolsLunarGHash;
+  #    fetchSubmodules = true;
+  #  };
+  #});
+  #vulkan-validation-layers = (prev.vulkan-validation-layers.override
+  #  { inherit (self) vulkan-headers spirv-headers; }).overrideAttrs (pa: with vulkanVersions; {
+  #  version = vulkanValidationLayersVersion;
+  #  src = final.fetchFromGitHub {
+  #    owner = "KhronosGroup";
+  #    repo = "Vulkan-ValidationLayers";
+  #    rev = vulkanValidationLayersRev;
+  #    hash = vulkanValidationLayersHash;
+  #  };
+  #  meta = pa.meta // { broken = vulkanValidationLayersVersion == "1.3.256" && vulkanHeadersVersion == "1.3.257"; };
+  #});
 }))
