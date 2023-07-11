dc84f3329c24e1936e137c5578539348375d62eb
Pedro Lara Campos <root@pedrohlc.com>
2023-07-11 21:29:20 +0000

    linux_hdr: rebase on linux_cachyos (#202)

--- pkgs/linux-cachyos/default.nix
+++ pkgs/linux-cachyos/default.nix
@@ -2,6 +2,7 @@
 , callPackage
 , fetchFromGitHub
 , fetchurl
+, hdrSupport ? false
 , lib
 , linuxManualConfig
 , stdenv
@@ -133,12 +134,12 @@ in
       name = builtins.baseNameOf filename;
       patch = filename;
     })
-    [
+    ([
       "${patches-src}/${major}/all/0001-cachyos-base-all.patch"
       "${patches-src}/${major}/sched/0001-EEVDF.patch"
       "${patches-src}/${major}/sched/0001-bore-eevdf.patch"
       "${patches-src}/${major}/misc/0001-Add-extra-version-CachyOS.patch"
-    ];
+    ] ++ lib.optional hdrSupport "${patches-src}/${major}/misc/0001-AMD-HDR.patch");
 
   extraMeta = { maintainers = with lib; [ maintainers.dr460nf1r3 ]; };
 }
