d71a7db1c947d1e1bc4155abd54c87a5e13e7031
PedroHLC ☭ <root@pedrohlc.com>
2023-07-12 19:50:21 -0300

    gamescope_git: remove no-longer necessary interferences

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -3,6 +3,4 @@
 prev.gamescope.overrideAttrs (pa: {
   version = nyxUtils.gitToVersion flakes.gamescope-git-src;
   src = flakes.gamescope-git-src;
-  patches = [ (final.lib.lists.take 1 pa.patches) ];
-  buildInputs = pa.buildInputs ++ (with final; [ glm gbenchmark ]);
 })
