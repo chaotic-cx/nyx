62b58d1fdd453ed4a0f922f41353f49d4c5d9dfe
PedroHLC ☭ <root@pedrohlc.com>
2023-07-15 19:31:33 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -5,7 +5,6 @@
   "linuxPackages_cachyos.lttng-modules" = "/nix/store/03awic8m6d72mfa4yxj4z6615lp9z3nv-lttng-modules-6.4.3-2.13.8";
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/0lv09fynqp41a0p2i8v6cv8gg8gm85gs-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/xb1j5sd45qrfdhmvbx2knl8kzkk0z01g-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_cachyos.openafs" = "/nix/store/90hx0ashfmnpa9fmw3rdiifrngjdvz7d-openafs-1.8.9-6.4.3-cachyos";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/rxrxx4ln8215himv43lq1mvmwfasd6y7-system76-acpi-module-1.0.2-6.4.3";
   "linuxPackages_cachyos.tp_smapi" = "/nix/store/9v5j3052qhdqff9017nvkzjnid1vx09h-tp_smapi-0.43-6.4.3";
   "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/w76zqrg5m7g1m270d92hjfmhz9dw9hmq-VirtualBox-GuestAdditions-7.0.8-6.4.3";
@@ -15,7 +14,6 @@
   "linuxPackages_hdr.lttng-modules" = "/nix/store/b4m9rwkgs2bpqrj9m778z47rpi0318mm-lttng-modules-6.4.3-2.13.8";
   "linuxPackages_hdr.mba6x_bl" = "/nix/store/gyxiw4sy0v05qzaq44blbyc3kpvr3x5h-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_hdr.mbp2018-bridge-drv" = "/nix/store/mfmx4pzyz8z5lg4dyc80splf3px2373z-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_hdr.openafs" = "/nix/store/zv0yhj5w4yg6db7c3v0spmj5pbrbmbfn-openafs-1.8.9-6.4.3-cachyos";
   "linuxPackages_hdr.system76-acpi" = "/nix/store/nd4iwzz430d9052qcb4hbi6a4w7741b5-system76-acpi-module-1.0.2-6.4.3";
   "linuxPackages_hdr.tp_smapi" = "/nix/store/z0zxxgpjb8zir6gddck660qmy2a0ng8g-tp_smapi-0.43-6.4.3";
   "linuxPackages_hdr.virtualboxGuestAdditions" = "/nix/store/c98fxr2ib4j3gzwbii9lfj942hy376w7-VirtualBox-GuestAdditions-7.0.8-6.4.3";
