d660f955ca3b62de8d1069c15b783aa59e79bca7
PedroHLC ☭ <root@pedrohlc.com>
2023-07-19 16:50:22 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -5,6 +5,9 @@
   "linuxPackages_cachyos.lttng-modules" = "/nix/store/d5n6cikiqsvycqhhldxn7bqd6rfvh2rs-lttng-modules-6.4.4-2.13.8";
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/napv1ymj55awkw6x0ln9iz2x42bcgvmg-mba6x_bl-unstable-2016-12-08";
   "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/maw7lxrv7b69w6dsq2l7j7y194f85mgw-mbp2018-bridge-drv-2020-01-31";
+  "linuxPackages_cachyos.rtl8812au" = "/nix/store/mhs7yzjf0pxwzg288cdpvnxnl1ilvaca-rtl8812au-6.4.4-unstable-2023-05-11";
+  "linuxPackages_cachyos.rtl8821au" = "/nix/store/ix29g4xkl39sh633f33fga7nm1vxn3wg-rtl8821au-6.4.4-unstable-2023-03-22";
+  "linuxPackages_cachyos.rtl88x2bu" = "/nix/store/57kzcssyznkynh6c0k67fq7cczbj42h4-rtl88x2bu-6.4.4-unstable-2023-03-17";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/5k98dxv132yh4sdf2awfg1wkpiammnr3-system76-acpi-module-1.0.2-6.4.4";
   "linuxPackages_cachyos.tp_smapi" = "/nix/store/0djkhgkn0jc0nj87zabqfzas08vqzss5-tp_smapi-0.43-6.4.4";
   "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/j5mdsn042bnk94vm444bgnji4shl7js7-VirtualBox-GuestAdditions-7.0.8-6.4.4";
