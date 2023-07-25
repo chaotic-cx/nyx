41be4e860efda2cd0c9b1e93c55b0c4f4e3dc7e4
PedroHLC ☭ <root@pedrohlc.com>
2023-07-25 11:03:01 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,14 +1,8 @@
 {
-  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/j61f5mf39kjs87c469zbvm3x3wjwyxvm-ddcci-driver-6.4.4-0.4.3";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/31afpm46xaqhq0hb9jcymqcn7ndb4rmh-dpdk-22.11.1-6.4.4";
-  "linuxPackages_cachyos.isgx" = "/nix/store/8qsq38hlz904rav5gf6vfwwzwkif2lf2-isgx-2.14-6.4.4";
-  "linuxPackages_cachyos.lttng-modules" = "/nix/store/4asa24cknyabl723l8r0b7q5nd99bz0c-lttng-modules-6.4.4-2.13.8";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/i00znkigggsqllw7y1qb6y1hrk83xhzx-mba6x_bl-unstable-2016-12-08";
-  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/zz5bnlcmdhxnrvbqhkr50aan5h23s45s-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_cachyos.rtl8812au" = "/nix/store/88dmh8jirznpg069i7kyqh16ld682gad-rtl8812au-6.4.4-unstable-2023-05-11";
-  "linuxPackages_cachyos.rtl8821au" = "/nix/store/jgfgva51j0ci9gvrqfhvhjjwvr82s1ig-rtl8821au-6.4.4-unstable-2023-03-22";
-  "linuxPackages_cachyos.rtl88x2bu" = "/nix/store/547cyrsyx5wsy3bpjjsd60p270kvnlwl-rtl88x2bu-6.4.4-unstable-2023-03-17";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/4n6gs2kwxiwzzqi3ah1n3c8nq2hcqav7-system76-acpi-module-1.0.2-6.4.4";
-  "linuxPackages_cachyos.tp_smapi" = "/nix/store/wdzvl6f3pdmb2kz9h0kxhqbfsw3aagkq-tp_smapi-0.43-6.4.4";
-  "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/5nkaz6l92lp376y0462rx30xnwx22i9z-VirtualBox-GuestAdditions-7.0.8-6.4.4";
+  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/0c3xnsk7nxyin84hkasf7nah38wpbb9s-ddcci-driver-6.4.4-0.4.3";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/2jbl7m6f0s71k5fwxfynf81ybl2is2mv-dpdk-22.11.1-6.4.4";
+  "linuxPackages_cachyos.isgx" = "/nix/store/88n3psflq97qr5i9sqy8fkjlfn8f3yns-isgx-2.14-6.4.4";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/3mmjjgdqgxn7vih8x991zdbgs8hkqg6z-mba6x_bl-unstable-2017-12-30";
+  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/va0cbm3sfqj7sqq97klv26s671yz8wkm-mbp2018-bridge-drv-2020-01-31";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/4csh3j829aykhj5kg9x6vp9irbqn1qpg-system76-acpi-module-1.0.2-6.4.4";
 }
