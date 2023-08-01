20fd639df62eb580f71b7d961ba1cf0c019b220a
PedroHLC ☭ <root@pedrohlc.com>
2023-08-01 10:58:04 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,6 +1,6 @@
 {
   "linuxPackages_cachyos.ddcci-driver" = "/nix/store/32hns5aiad33nscz8vs9sg1x6x2clxyl-ddcci-driver-6.4.7-0.4.3";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/x5fv2knl8zzp13kiyh7pl42azq5v7pj1-dpdk-22.11.1-6.4.7";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/fpv0w1blxd2sxnm6jzximgk40shhclcw-dpdk-22.11.1-6.4.7";
   "linuxPackages_cachyos.isgx" = "/nix/store/007w7n6hsll1apzanaypcy947pzzvh5i-isgx-2.14-6.4.7";
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/ivlmk4bjk4kb1lhdxvfvmdimdvn29x65-mba6x_bl-unstable-2017-12-30";
   "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/y8cnxilv571dwq1d8mr4qjbbrxrfyrpc-mbp2018-bridge-drv-2020-01-31";
