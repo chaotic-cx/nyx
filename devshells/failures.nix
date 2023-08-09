b79cbcde446c0731d99f5557f430fe00d2d54e00
PedroHLC ☭ <root@pedrohlc.com>
2023-08-09 11:16:15 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,8 +1,8 @@
 {
-  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/9qnl5i24j8nl2js41rkl49l2aa3v80mz-ddcci-driver-6.4.8-0.4.3";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/4i4x4n668y8yp2v53hsff53xwsb5k5zi-dpdk-22.11.1-6.4.8";
-  "linuxPackages_cachyos.isgx" = "/nix/store/ajvnq4b4v5ckcc3pysmz6ynazkrfl3q0-isgx-2.14-6.4.8";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/0df47c274ilgv3cbadgw5pq0gvqxz78p-mba6x_bl-unstable-2017-12-30";
-  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/43q4l919mbz3nkg0m7y4bds910awclnl-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/2dkxn3xcpw0qim5hkzyyw00f93yy5i3c-system76-acpi-module-1.0.2-6.4.8";
+  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/3dsbpdanw2gy7jby7bl7s5gy38i4w3s5-ddcci-driver-6.4.9-0.4.3";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/rks4xvc653gb3q0ql37nixjfnlrcl970-dpdk-22.11.1-6.4.9";
+  "linuxPackages_cachyos.isgx" = "/nix/store/32fj9zgg5rc1jzkzvikli08hwn94yl2p-isgx-2.14-6.4.9";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/4p3ssrv7969alvlv9v2w7ga155naijzq-mba6x_bl-unstable-2017-12-30";
+  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/a968hgp0m2hw3jzanf5zfqqgxfs60vny-mbp2018-bridge-drv-2020-01-31";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/hrd1rgy3k1n9ifa0i7ip94qr8q5d68j0-system76-acpi-module-1.0.2-6.4.9";
 }
