c815cb547f921a850ec32a5f83a94fcf0317b490
PedroHLC ☭ <root@pedrohlc.com>
2023-08-12 11:00:08 -0300

    input-leap_git: rebase over nixpkgs' impl

--- pkgs/input-leap-git/default.nix
+++ pkgs/input-leap-git/default.nix
@@ -1,29 +1,16 @@
-{ final, flakes, nyxUtils, prev, qttools, ... }:
+{ final, flakes, nyxUtils, ... }:
 
-prev.barrier.overrideAttrs (pa: {
-  pname = "input-leap";
+final.input-leap.overrideAttrs (pa: {
   src = flakes.input-leap-git-src;
   version = nyxUtils.gitToVersion flakes.input-leap-git-src;
   nativeBuildInputs = pa.nativeBuildInputs ++ (with final; [
-    pkg-config
     gtest
     ghc_filesystem
   ]);
-  buildInputs = pa.buildInputs ++ [
-    final.libei
-    final.libportal
-    qttools
-  ];
-  patches = [ ];
+  buildInputs = pa.buildInputs ++ (with final; [
+    libuuid
+  ]);
   cmakeFlags = [
     "-DINPUTLEAP_USE_EXTERNAL_GTEST=ON"
-    "-DINPUTLEAP_BUILD_LIBEI=ON"
   ];
-  postFixup = ''
-    substituteInPlace "$out/share/applications/input-leap.desktop" --replace "Exec=input-leap" "Exec=$out/bin/input-leap"
-  '';
-  meta = pa.meta // {
-    description = "Input-leap is KVM software forked from Symless's synergy 1.9 codebase.";
-    homepage = "https://github.com/input-leap/input-leap";
-  };
 })
