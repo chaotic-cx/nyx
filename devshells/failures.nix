d0f5d569d27bc13e011991b3c8aef460b8236b51
PedroHLC ☭ <root@pedrohlc.com>
2023-08-12 11:36:19 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -5,4 +5,6 @@
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/g73d1hpwalvjpy777880mhd71q7p87ah-mba6x_bl-unstable-2017-12-30";
   "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/pckqq76vcyh3hhzzih83rbhfif7rpg4z-mbp2018-bridge-drv-2020-01-31";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/gb06jx9a8bh2l3varwp1rfklkw6vlclz-system76-acpi-module-1.0.2-6.4.10";
+  "linuxPackages_cachyos.virtualbox" = "/nix/store/ds0nbys3pwgd47dzqjj9mnammw0kpfq6-virtualbox-modules-7.0.10-6.4.10";
+  "linuxPackages_cachyos.vmware" = "/nix/store/yhs7n0wsxki3mjzyjxf6rg087qpg5rmx-vmware-modules-17.0.0-6.4.10";
 }
