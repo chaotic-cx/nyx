5a33dbda93d170b83b6bde5707936673d3392ffc
PedroHLC ☭ <root@pedrohlc.com>
2023-08-14 10:53:21 -0300

    mangohud_git: remove merged patch

--- pkgs/mangohud-git/default.nix
+++ pkgs/mangohud-git/default.nix
@@ -5,7 +5,10 @@ nyxUtils.multiOverride prev.mangohud { inherit mangohud32; }
     version = nyxUtils.gitToVersion src;
     src = flakes.mangohud-git-src;
     patches = [ ./preload-nix-workaround.patch ] ++
-      (nyxUtils.removeByBaseName "preload-nix-workaround.patch" prevAttrs.patches);
+      (nyxUtils.removeByBaseName "preload-nix-workaround.patch"
+        (nyxUtils.removeByURL "https://github.com/flightlessmango/MangoHud/commit/3f8f036ee8773ae1af23dd0848b6ab487b5ac7de.patch"
+          prevAttrs.patches
+        ));
     postPatch = (prevAttrs.postPatch or "") + ''
       substituteInPlace src/meson.build \
         --replace "run_command(['git', 'describe', '--tags', '--dirty=+']).stdout().strip()" \
