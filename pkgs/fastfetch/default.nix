b22ee3dba75580fbebf20ec69ae2fba2a8c33c05
Nico Jensch <root@dr460nf1r3.org>
2023-08-17 10:12:25 +0200

    fastfetch: update to 2.0.0

--- pkgs/fastfetch/default.nix
+++ pkgs/fastfetch/default.nix
@@ -1,22 +1,27 @@
-# Credits going to federicoschonborn, taken from NUR
-{ cmake
-, fetchFromGitHub
-, git
-, lib
-, nyxUtils
-, pkg-config
+# Credits going to federicoschonborn on NUR, 
+# the original Nix file has been sourced from there.
+{ lib
 , stdenv
-, enableLibpci ? false
-, pciutils
-, enableVulkan ? false
-, vulkan-loader
-, enableWayland ? false
-, libffi
-, wayland
-, enableXcb ? false
-, enableXrandr ? false
-, enableX11 ? false
-, xorg
+, fetchFromGitHub
+, fetchpatch
+, cmake
+, darwin
+, ninja
+, pkg-config
+, yyjson
+
+, enableChafa ? false
+, chafa
+, enableDbus ? false
+, dbus
+, enableDconf ? false
+, dconf
+, enableDdcutil ? false
+, ddcutil
+, enableEgl ? false
+, libGL
+, enableFreetype ? false
+, freetype
 , enableGio ? false
 , glib
 , libselinux
@@ -24,83 +29,95 @@
 , pcre16
 , pcre2
 , utillinux
-, enableDconf ? false
-, dconf
-, enableDbus ? false
-, dbus
-, enableXfconf ? false
-, xfce
-, enableSqlite3 ? false
-, sqlite
-, enableRpm ? false
-, rpm
-, enableImagemagick7 ? false
-, imagemagick
-, enableZlib ? false
-, zlib
-, enableChafa ? false
-, chafa
-, enableEgl ? false
-, libGL
 , enableGlx ? false
 , libglvnd
+, enableImagemagick ? false
+, imagemagick
+, enableLibnm ? false
+, networkmanager
+, enableLibpci ? false
+, pciutils
 , enableMesa ? false
-, mesa_drivers
+, mesa
 , enableOpencl ? false
 , ocl-icd
 , opencl-headers
-, enableLibcjson ? false
-, cjson
-, enableLibnm ? false
-, networkmanager
-, enableFreetype ? false
-, freetype
 , enablePulse ? false
-, libpulseaudio
-,
+, pulseaudio
+, enableRpm ? false
+, rpm
+, enableSqlite3 ? false
+, sqlite
+, enableVulkan ? false
+, vulkan-loader
+, enableWayland ? false
+, libffi
+, wayland
+, enableX11 ? false
+, xorg
+, enableXcb ? false
+, enableXfconf ? false
+, xfce
+, enableXrandr ? false
+, enableZlib ? false
+, zlib
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "1.12.2";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "LinusDierheimer";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-l9fIm7+dBsOqGoFUYtpYESAjDy3496rDTUDQjbNU4U0=";
+    hash = "sha256-mXbkzPlX1OsK+ahUSJWktV5D7Mo2zkhXgXP54QjbIR4=";
   };
+
+  patches = [
+    # Do not fetch yyjson
+    (fetchpatch {
+      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/app-misc/fastfetch/files/fastfetch-2.0.0-dont-fetch-yyjson.patch";
+      hash = "sha256-mOykwXSuad8BrUBmjX39EmQb0/hnKezgmWe8cpAybsw=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
-    git
+    ninja
     pkg-config
   ];
 
-  buildInputs =
-    [ ]
-    ++ lib.optionals enableChafa [ chafa ]
-    ++ lib.optionals enableDbus [ dbus ]
-    ++ lib.optionals enableDconf [ dconf ]
-    ++ lib.optionals enableEgl [ libGL ]
-    ++ lib.optionals enableFreetype [ freetype ]
-    ++ lib.optionals enableGio [ glib libselinux libsepol pcre16 pcre2 utillinux ]
-    ++ lib.optionals enableGlx [ libglvnd ]
-    ++ lib.optionals enableImagemagick7 [ imagemagick ]
-    ++ lib.optionals enableLibcjson [ cjson ]
-    ++ lib.optionals enableLibnm [ networkmanager ]
-    ++ lib.optionals enableLibpci [ pciutils ]
-    ++ lib.optionals enableMesa [ mesa_drivers.dev ]
-    ++ lib.optionals enableOpencl [ ocl-icd opencl-headers ]
-    ++ lib.optionals enablePulse [ libpulseaudio ]
-    ++ lib.optionals enableRpm [ rpm ]
-    ++ lib.optionals enableSqlite3 [ sqlite ]
-    ++ lib.optionals enableVulkan [ vulkan-loader ]
-    ++ lib.optionals enableWayland [ libffi wayland ]
-    ++ lib.optionals enableX11 [ xorg.libX11 ]
-    ++ lib.optionals enableXcb [ xorg.libXau xorg.libXdmcp xorg.libxcb ]
-    ++ lib.optionals enableXfconf [ xfce.xfconf ]
-    ++ lib.optionals enableXrandr [ xorg.libXext xorg.libXrandr ]
-    ++ lib.optionals enableZlib [ zlib ];
+  buildInputs = [
+    yyjson
+  ]
+  ++ lib.optionals enableChafa [ chafa ]
+  ++ lib.optionals enableImagemagick [ imagemagick ]
+  ++ lib.optionals enableSqlite3 [ sqlite ]
+  ++ lib.optionals enableVulkan ([ vulkan-loader ] ++ lib.optionals stdenv.isDarwin [ darwin.moltenvk ])
+  ++ lib.optionals enableZlib [ zlib ]
+  ++ (
+    lib.optionals (!stdenv.isDarwin) ([ ]
+      ++ lib.optionals enableDbus [ dbus ]
+      ++ lib.optionals enableDconf [ dconf ]
+      ++ lib.optionals enableDdcutil [ ddcutil ]
+      ++ lib.optionals enableEgl [ libGL ]
+      ++ lib.optionals enableFreetype [ freetype ]
+      # glib depends on pcre2 and libselinux depends on pcre16
+      ++ lib.optionals enableGio [ glib libselinux libsepol pcre16 pcre2 utillinux ]
+      ++ lib.optionals enableGlx [ libglvnd ]
+      ++ lib.optionals enableLibnm [ networkmanager ]
+      ++ lib.optionals enableLibpci [ pciutils ]
+      ++ lib.optionals enableMesa [ mesa ]
+      ++ lib.optionals enableOpencl [ ocl-icd opencl-headers ]
+      ++ lib.optionals enablePulse [ pulseaudio ]
+      ++ lib.optionals enableRpm [ rpm ]
+      ++ lib.optionals enableWayland [ libffi wayland ]
+      ++ lib.optionals enableX11 [ xorg.libX11 ]
+      ++ lib.optionals enableXcb [ xorg.libXau xorg.libXdmcp xorg.libxcb ]
+      ++ lib.optionals enableXfconf [ xfce.xfconf ]
+      ++ lib.optionals enableXrandr [ xorg.libXext xorg.libXrandr ])
+  );
 
   cmakeFlags = [
     "-DTARGET_DIR_ROOT=${placeholder "out"}"
