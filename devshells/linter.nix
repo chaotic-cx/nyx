59c91ea4a92bd9bfc3fcdecf8b60ff9dd3e0b080
PedroHLC ☭ <root@pedrohlc.com>
2023-08-23 14:34:44 -0300

    linter: improve

--- devshells/linter.nix
+++ devshells/linter.nix
@@ -1,4 +1,5 @@
-{ findutils
+{ deadnix
+, findutils
 , nixpkgs-fmt
 , ripgrep
 , statix
@@ -9,13 +10,21 @@ let
   rg = "${p.ripgrep}/bin/rg";
   fmt = "${p.nixpkgs-fmt}/bin/nixpkgs-fmt";
   statix = "${p.statix}/bin/statix";
+  deadnix = "${p.deadnix}/bin/deadnix";
 in
 writeShellScriptBin "chaotic-nyx-lint" ''
   set -euo pipefail
 
+  echo "Running nixpkgs-fmt..."
   ${fmt} --check .
+
+  echo "Running statix..."
   ${statix} check
 
+  echo "Running deadnix..."
+  ${deadnix} --fail .
+
+  echo "Searching ugly code..."
   _SHORT_FILES=$(${find} . -type f -name '*.nix' | ${rg} -P '[^\w"@\(](?!url|src|drv|xs|id|_?[kvxn])([a-zA-Z_][a-zA-Z]{0,2}):')
   _SHORT_FILES_EXIT=$?
   if [ -eq _SHORT_FILES_EXIT 0 ]; then
