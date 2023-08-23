ac2a6914b7c78528e62bb3244d27c5666786296b
Nico <github@dr460nf1r3.org>
2023-08-23 12:17:13 +0200

    fastfetch: update to 2.0.2 (#279)

--- pkgs/fastfetch/default.nix
+++ pkgs/fastfetch/default.nix
@@ -65,22 +65,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.0.0";
+  version = "2.0.2";
 
   src = fetchFromGitHub {
     owner = "LinusDierheimer";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-mXbkzPlX1OsK+ahUSJWktV5D7Mo2zkhXgXP54QjbIR4=";
+    hash = "sha256-dWeJ+sLZrnnhXyuPoOCsEiqLabavbXgAUkqZJ5Ff0XY=";
   };
-
-  patches = [
-    # Do not fetch yyjson
-    (fetchpatch {
-      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/app-misc/fastfetch/files/fastfetch-2.0.0-dont-fetch-yyjson.patch";
-      hash = "sha256-mOykwXSuad8BrUBmjX39EmQb0/hnKezgmWe8cpAybsw=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
