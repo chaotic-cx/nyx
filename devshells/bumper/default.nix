0d890426032e853a149946176f7cb1f400be2b51
PedroHLC ☭ <root@pedrohlc.com>
2023-08-23 15:01:06 -0300

    linter: improve (2)

--- devshells/bumper/default.nix
+++ devshells/bumper/default.nix
@@ -1,16 +1,19 @@
 { coreutils
+, gh
+, git
 , gnused
+, nix
 , ripgrep
 , update-scripts
 , writeShellScriptBin
-}@p:
+}:
 let
-  git = "${p.git}/bin/git";
-  nix = "${p.nix}/bin/nix";
-  date = "${p.coreutils}/bin/date";
-  grep = "${p.ripgrep}/bin/rg";
-  sed = "${p.gnused}/bin/sed";
-  gh = "${p.gh}/bin/gh";
+  Git = "${git}/bin/git";
+  Nix = "${nix}/bin/nix";
+  Date = "${coreutils}/bin/date";
+  Grep = "${ripgrep}/bin/rg";
+  Sed = "${gnused}/bin/sed";
+  Gh = "${gh}/bin/gh";
 in
 writeShellScriptBin "chaotic-nyx-bumper" ''
   #!/usr/bin/env bash
@@ -24,24 +27,24 @@ writeShellScriptBin "chaotic-nyx-bumper" ''
   }
 
   BUMPN=''${BUMPN:-1}
-  NAME=''${NAME:-$(${date} '+%Y%m%d')-$BUMPN}
+  NAME=''${NAME:-$(${Date} '+%Y%m%d')-$BUMPN}
   BRANCH=''${BRANCH:-bump/$NAME}
 
   function checkout() {
-    ${git} checkout -b "$BRANCH"
-    ${git} fetch origin
-    ${git} reset --hard origin/main
+    ${Git} checkout -b "$BRANCH"
+    ${Git} fetch origin
+    ${Git} reset --hard origin/main
     return 0
   }
 
   function bump-flake() {
-    ${nix} flake update
+    ${Nix} flake update
     CHANGED=()
-    readarray -t CHANGED < <(${git} diff | ${grep} -Po '(?<=^     ")([^"]+)(?=": {$)' | ${sed} 's/-src$//;s/-git$//')
+    readarray -t CHANGED < <(${Git} diff | ${Grep} -Po '(?<=^     ")([^"]+)(?=": {$)' | ${Sed} 's/-src$//;s/-git$//')
     [ -z "$CHANGED" ] && return 0
     CHANGED_CSV=$(join_by ', ' "''${CHANGED[@]}")
-    ${git} add -u
-    ${git} commit -m "flake-''${NAME}: $CHANGED_CSV"
+    ${Git} add -u
+    ${Git} commit -m "flake-''${NAME}: $CHANGED_CSV"
     return 0
   }
 
@@ -50,11 +53,11 @@ writeShellScriptBin "chaotic-nyx-bumper" ''
   }
 
   function push() {
-    ${git} push origin "$BRANCH" -u
+    ${Git} push origin "$BRANCH" -u
   }
 
   function create-pr() {
-    ${gh} pr create -B main -H "$BRANCH" \
+    ${Gh} pr create -B main -H "$BRANCH" \
       --title "Bump $NAME" \
       --body 'Bump our packages since we do this daily.'
   }
--- devshells/linter.nix
+++ devshells/linter.nix
@@ -4,30 +4,29 @@
 , ripgrep
 , statix
 , writeShellScriptBin
-}@p:
+}:
 let
-  find = "${p.findutils}/bin/find";
-  rg = "${p.ripgrep}/bin/rg";
-  fmt = "${p.nixpkgs-fmt}/bin/nixpkgs-fmt";
-  statix = "${p.statix}/bin/statix";
-  deadnix = "${p.deadnix}/bin/deadnix";
+  Find = "${findutils}/bin/find";
+  Rg = "${ripgrep}/bin/rg";
+  Fmt = "${nixpkgs-fmt}/bin/nixpkgs-fmt";
+  Statix = "${statix}/bin/statix";
+  Deadnix = "${deadnix}/bin/deadnix";
 in
 writeShellScriptBin "chaotic-nyx-lint" ''
   set -euo pipefail
 
   echo "Running nixpkgs-fmt..."
-  ${fmt} --check .
+  ${Fmt} --check .
 
   echo "Running statix..."
-  ${statix} check
+  ${Statix} check
 
   echo "Running deadnix..."
-  ${deadnix} --fail .
+  ${Deadnix} --fail .
 
   echo "Searching ugly code..."
-  _SHORT_FILES=$(${find} . -type f -name '*.nix' | ${rg} -P '[^\w"@\(](?!url|src|drv|xs|id|_?[kvxn])([a-zA-Z_][a-zA-Z]{0,2}):')
-  _SHORT_FILES_EXIT=$?
-  if [ -eq _SHORT_FILES_EXIT 0 ]; then
+  _SHORT_FILES=$(${Find} . -type f -name '*.nix' | (xargs ${Rg} -P '[^\w"@\(](?!url|src|drv|xs|id|_?[kvxn])([a-zA-Z_][a-zA-Z]{0,2}):' || true))
+  if [[ -n "$_SHORT_FILES" ]]; then
     echo "Lambda parameters can't have three letters or less (except: url, src, drv, x, xs, id, k, v, n):"
     echo "$_SHORT_FILES"
   fi
