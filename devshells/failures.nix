c386925454689482cdb18ed387f843d84cda952f
PedroHLC ☭ <root@pedrohlc.com>
2023-08-23 21:04:58 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,10 +1,10 @@
 {
-  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/wfmh8swg0s21qhz1xhksy6p40404kllp-ddcci-driver-6.4.11-0.4.3";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/kckyd1vn3dmhy4g820s1d34bf456xwsk-dpdk-22.11.1-6.4.11";
-  "linuxPackages_cachyos.isgx" = "/nix/store/046d9sij4pi7lm6njhj90hzxj6f1z3pf-isgx-2.14-6.4.11";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/0qq4175gqyazhqjdx0rh76qwrbryk17s-mba6x_bl-unstable-2017-12-30";
-  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/hnsvn1449pv3vpwjb7mk5wc73q53yyi3-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/35whycf74f74y6lmlcfrx4y1ph1w0k8v-system76-acpi-module-1.0.2-6.4.11";
-  "linuxPackages_cachyos.virtualbox" = "/nix/store/gb04xxn9wla4lmmgrbzp4p9whzxqa8b2-virtualbox-modules-7.0.10-6.4.11";
-  "linuxPackages_cachyos.vmware" = "/nix/store/59yycw9j6bgzb13b934frm7ka0ykq8iq-vmware-modules-17.0.0-6.4.11";
+  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/7y4v0843xxfnlk9rcwbdpx3hpk5sblvd-ddcci-driver-6.4.12-0.4.3";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/yihqygwkqxfkzwkz02jp55j2g5vjs631-dpdk-22.11.1-6.4.12";
+  "linuxPackages_cachyos.isgx" = "/nix/store/k4w0mh1c17297cxanrgsflayixbdmpp4-isgx-2.14-6.4.12";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/77x7ryfhvyrfns6jvjp7kib4nng67w6g-mba6x_bl-unstable-2017-12-30";
+  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/13y6wqqa2dr1bwhpskaml6w61bn72glx-mbp2018-bridge-drv-2020-01-31";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/nsn9v123sczlc67j53462xp30jx65mkf-system76-acpi-module-1.0.2-6.4.12";
+  "linuxPackages_cachyos.virtualbox" = "/nix/store/yxb2wfk92z4l51dd1qsyi6hsbiz90r87-virtualbox-modules-7.0.10-6.4.12";
+  "linuxPackages_cachyos.vmware" = "/nix/store/awvlrz3bzypfc99fc5kaqwpsa603d6ik-vmware-modules-17.0.0-6.4.12";
 }
