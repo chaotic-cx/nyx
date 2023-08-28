9ed7212a26d4ca54dc2121fddef9ad38b13471b0
PedroHLC ☭ <root@pedrohlc.com>
2023-08-28 16:48:03 -0300

    failures: update

--- devshells/failures.nix
+++ devshells/failures.nix
@@ -1,10 +1,17 @@
 {
-  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/7y4v0843xxfnlk9rcwbdpx3hpk5sblvd-ddcci-driver-6.4.12-0.4.3";
-  "linuxPackages_cachyos.dpdk" = "/nix/store/69kk5w069d3p4qxip5chp19cmad99gnr-dpdk-22.11.1-6.4.12";
-  "linuxPackages_cachyos.isgx" = "/nix/store/k4w0mh1c17297cxanrgsflayixbdmpp4-isgx-2.14-6.4.12";
-  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/77x7ryfhvyrfns6jvjp7kib4nng67w6g-mba6x_bl-unstable-2017-12-30";
-  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/13y6wqqa2dr1bwhpskaml6w61bn72glx-mbp2018-bridge-drv-2020-01-31";
-  "linuxPackages_cachyos.system76-acpi" = "/nix/store/nsn9v123sczlc67j53462xp30jx65mkf-system76-acpi-module-1.0.2-6.4.12";
-  "linuxPackages_cachyos.virtualbox" = "/nix/store/c2g1ahm50qdjslq9qkk9iwxm88zi3pqa-virtualbox-modules-7.0.10-6.4.12";
-  "linuxPackages_cachyos.vmware" = "/nix/store/awvlrz3bzypfc99fc5kaqwpsa603d6ik-vmware-modules-17.0.0-6.4.12";
+  "linuxPackages_cachyos.apfs" = "/nix/store/660yxbsmd9vpnmkikgv6b7wgkd24fl85-apfs-0.3.3-6.5.0";
+  "linuxPackages_cachyos.cryptodev" = "/nix/store/3w8islqmvjrms4fv7m0m45sc2l1ar65z-cryptodev-linux-1.13-6.5.0";
+  "linuxPackages_cachyos.ddcci-driver" = "/nix/store/nxss0h84cfxhsf30q2ba1c92czjvy7vm-ddcci-driver-6.5.0-0.4.3";
+  "linuxPackages_cachyos.dpdk" = "/nix/store/p8cw6lnnlmlld7ni1fvlp29n0d352y2r-dpdk-22.11.1-6.5.0";
+  "linuxPackages_cachyos.ipu6-drivers" = "/nix/store/lw3vrwww6vwnnhs28170sckkiza6svaa-ipu6-drivers-unstable-2023-06-02";
+  "linuxPackages_cachyos.isgx" = "/nix/store/k0jfd3lsiqd317ijjjpywr8fmnwddxil-isgx-2.14-6.5.0";
+  "linuxPackages_cachyos.kvdo" = "/nix/store/g5lf6dc8m70f1jp52jqj5riwabbga3vw-kvdo-8.2.1.6";
+  "linuxPackages_cachyos.mba6x_bl" = "/nix/store/wdbi29p3mz09qqksizh9x88zvffb67p2-mba6x_bl-unstable-2017-12-30";
+  "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/y90i88qs3nav7skqxa6y9xqlg159k6dj-mbp2018-bridge-drv-2020-01-31";
+  "linuxPackages_cachyos.openafs" = "/nix/store/z3c5dr0sgkmlzqk9m7cvz6hfl8vli5la-openafs-1.8.10-6.5.0-cachyos";
+  "linuxPackages_cachyos.rtl8821cu" = "/nix/store/jh4nmxlw27m48ddi5id071814jfhh2kj-rtl8821cu-6.5.0-unstable-2023-04-28";
+  "linuxPackages_cachyos.system76-acpi" = "/nix/store/3b1vwl1xb8kvm8p8qdh0ay8qbgy0x49r-system76-acpi-module-1.0.2-6.5.0";
+  "linuxPackages_cachyos.virtio_vmmci" = "/nix/store/qx32inrznjk34x6h3h8i2k85n8qbj033-virtio_vmmci";
+  "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/h9bk44sqx2mszg15h7ikh036l6kv6i7i-VirtualBox-GuestAdditions-7.0.10-6.5.0";
+  "linuxPackages_cachyos.vmware" = "/nix/store/a8z37mzjn1qc091zbqj513dj1rbf5cnp-vmware-modules-17.0.0-6.5.0";
 }
