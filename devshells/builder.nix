9a357eda3577fbd27a9ae5ba02c23033d1342cd0
PedroHLC ☭ <root@pedrohlc.com>
2023-08-30 13:41:02 -0300

    builder: allow resync

--- devshells/builder.nix
+++ devshells/builder.nix
@@ -184,7 +184,7 @@ writeShellScriptBin "chaotic-nyx-build" ''
   if [ -z "$CACHIX_AUTH_TOKEN" ] && [ -z "$CACHIX_SIGNING_KEY" ]; then
     echo_error "No key for cachix -- failing to deploy."
     exit 23
-  elif [ -s push.txt ]; then
+  elif [ -n "''${NYX_RESYNC:-}" ] || [ -s push.txt ]; then
     # Let nix digest store paths first
     sleep 10
 
