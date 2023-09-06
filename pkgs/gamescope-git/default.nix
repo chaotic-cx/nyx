07b7096da581ab8438053f014cd234fb185d142b
PedroHLC ☭ <root@pedrohlc.com>
2023-09-06 19:25:53 -0300

    gamescope_git: migrate to nyx-generic-git-update

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -1,6 +1,14 @@
-{ flakes, nyxUtils, prev, ... }:
+{ final, prev, gitOverride, ... }:
 
-prev.gamescope.overrideAttrs (_prevAttrs: {
-  version = nyxUtils.gitToVersion flakes.gamescope-git-src;
-  src = flakes.gamescope-git-src;
-})
+gitOverride {
+  nyxKey = "gamescope_git";
+  versionNyxPath = "pkgs/gamescope-git/version.json";
+  versionLocalPath = ./version.json;
+  prev = prev.gamescope;
+  fetcher =
+    _prevAttrs: finalArgs: final.fetchFromGitHub ({
+      owner = "ValveSoftware";
+      repo = "gamescope";
+    } // finalArgs);
+  fetchLatestRev = src: final.callPackage ../../shared/github-rev-fetcher.nix { inherit src; ref = "master"; };
+}
