90209ab2df6d072bb1f3f58a79d98f5ff77eef21
PedroHLC ☭ <root@pedrohlc.com>
2023-09-11 13:28:16 -0300

    firefox_nightly: init; nss_git: init

---
+++ pkgs/firefox-nightly/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, current ? importJSON ./version.json
+, importJSON ? lib.trivial.importJSON
+, buildMozillaMach
+, callPackage
+, fetchurl
+, nss_git
+, nyxUtils
+, stdenv
+}:
+
+let
+  mach = buildMozillaMach
+    rec {
+      pname = "firefox-nightly";
+      inherit (current) version;
+      applicationName = "Mozilla Firefox Nightly";
+      branding = "browser/branding/nightly";
+      src = fetchurl {
+        inherit (current) hash;
+        url = "https://hg.mozilla.org/mozilla-central/archive/${current.rev}.zip";
+      };
+
+      meta = {
+        description = "A web browser built from Firefox Nightly source tree";
+        homepage = "http://www.mozilla.com/en-US/firefox/";
+        maintainers = with lib.maintainers; [ pedrohlc ];
+        platforms = lib.platforms.unix;
+        badPlatforms = lib.platforms.darwin;
+        broken = stdenv.buildPlatform.is32bit;
+        maxSilent = 14400;
+        license = lib.licenses.mpl20;
+        mainProgram = "firefox";
+      };
+
+      # updateScript = callPackage ./update.nix { };
+    };
+
+  noKey = prev: {
+    env.MOZ_REQUIRE_SIGNING = "";
+  };
+in
+nyxUtils.multiOverride mach { nss_latest = nss_git; } noKey
---
+++ pkgs/nss-git/default.nix
@@ -0,0 +1,24 @@
+{ final, prev, gitOverride, nyxUtils, ... }:
+
+gitOverride {
+  nyxKey = "nss_git";
+  versionNyxPath = "pkgs/nss-git/version.json";
+  versionLocalPath = ./version.json;
+  prev = prev.nss_latest;
+  fetcher =
+    _prevAttrs: finalArgs: final.fetchFromGitHub ({
+      owner = "nss-dev";
+      repo = "nss";
+    } // finalArgs);
+  fetchLatestRev = src: final.callPackage ../../shared/github-rev-fetcher.nix { inherit src; ref = "master"; };
+  postOverrides = [
+    (prevAttrs: {
+      # they could have used "sourceRoot"...
+      postUnpack = ''
+        mkdir _nss
+        mv source/* _nss/
+        mv _nss source/nss
+      '';
+    })
+  ];
+}
