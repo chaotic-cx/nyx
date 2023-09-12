a2389357d79655204332eb81c6d7dde87569cb0f
PedroHLC ☭ <root@pedrohlc.com>
2023-09-12 15:32:34 -0300

    oops

--- modules/nixos/appmenu-gtk3-module.nix
+++ modules/nixos/appmenu-gtk3-module.nix
@@ -15,7 +15,7 @@ in
   config = lib.mkIf cfg.enable {
     environment = {
       systemPackages = with pkgs; [ appmenu-gtk3-module ];
-      sessionVariable.XDG_DATA_DIRS = [
+      sessionVariables.XDG_DATA_DIRS = [
         "${pkgs.appmenu-gtk3-module}/share/gsettings-schemas/appmenu-gtk3-module-0.7.6"
       ];
       variables.UBUNTU_MENUPROXY = "1";
