8ad4d368d6e5f6c1a7400f158ba5269f54e870e0
Pedro Lara Campos <root@pedrohlc.com>
2023-09-12 14:37:10 -0300

    Fixes 113f4b09c1bce4389028e417101a531e316518c3 (#320)

--- modules/nixos/appmenu-gtk3-module.nix
+++ modules/nixos/appmenu-gtk3-module.nix
@@ -13,12 +13,12 @@ in
       };
   };
   config = lib.mkIf cfg.enable {
-    environment.sessionVariables = {
-      XDG_DATA_DIRS = [ "${pkgs.appmenu-gtk3-module}/share/gsettings-schemas/appmenu-gtk3-module-0.7.6" ];
+    environment = {
+      systemPackages = with pkgs; [ appmenu-gtk3-module ];
+      sessionVariable.XDG_DATA_DIRS = [
+        "${pkgs.appmenu-gtk3-module}/share/gsettings-schemas/appmenu-gtk3-module-0.7.6"
+      ];
+      variables.UBUNTU_MENUPROXY = "1";
     };
-
-    environment.systemPackages = with pkgs; [ appmenu-gtk3-module ];
-
-    environment.variables.UBUNTU_MENUPROXY = "1";
   };
 }
