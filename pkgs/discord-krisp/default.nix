5aebfe7de409abd64443978f09b70cd973c9cec9
TNE <tne@garudalinux.org>
2023-10-02 06:07:23 +0200

    discord-krisp: init

---
+++ pkgs/discord-krisp/default.nix
@@ -0,0 +1,21 @@
+{ prev, ... }:
+let
+  patch-krisp = prev.writeScript "patch-krisp" ''
+    discord_version="${prev.discord.version}"
+    file="$HOME/.config/discord/$discord_version/modules/discord_krisp/discord_krisp.node"
+    if [ -f "$file" ]; then
+    addr=$("${prev.rizin}/bin/rz-find" -x '4881ec00010000' "$file" | head -n1)
+    "${prev.rizin}/bin/rizin" -q -w -c "s $addr + 0x30 ; wao nop" "$file"
+    fi
+  '';
+  binaryName = "Discord";
+in
+prev.discord.overrideAttrs (_: previousAttrs: {
+  postInstall = previousAttrs.postInstall + ''
+    wrapProgramShell $out/opt/${binaryName}/${binaryName} \
+    --run "${patch-krisp}"
+  '';
+  meta = {
+    nyx.bypassLicense = true;
+  };
+})
