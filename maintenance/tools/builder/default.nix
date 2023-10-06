293cb2ec29b284eada192adf0f64fe4a05a40dfc
Pedro Lara Campos <root@pedrohlc.com>
2023-10-06 16:32:24 -0300

    builder: put script in a script (#371)

--- maintenance/tools/builder/default.nix
+++ maintenance/tools/builder/default.nix
@@ -15,11 +15,16 @@
 , hostPlatform
 }:
 let
-  Jq = "${jq}/bin/jq";
-  Nix = "${nix}/bin/nix";
-  Grep = "${gnugrep}/bin/grep";
-  Cachix = "${cachix}/bin/cachix";
-  Xargs = "${findutils}/bin/xargs";
+  path = lib.makeBinPath [
+    coreutils-full
+    # cachix requires "git" in PATH
+    git
+    jq
+    nix
+    gnugrep
+    cachix
+    findutils
+  ];
 
   allPackagesList =
     builtins.map (xsx: xsx.drv)
@@ -100,139 +105,29 @@ let
 in
 writeShellScriptBin "chaotic-nyx-build" ''
   # Cleanup PATH for reproducibility.
-  # But, Cachix needs git in PATH.
-  PATH="${coreutils-full}/bin:${git}/bin"
+  PATH="${path}"
 
-  # Options
+  # Options (1)
   NYX_SOURCE="''${NYX_SOURCE:-${flakeSelf}}"
-  NYX_FLAGS="''${NYX_FLAGS:---accept-flake-config --no-link}"
-  NYX_WD="''${NYX_WD:-$(mktemp -d)}"
+  NYX_TARGET="''${NYX_TARGET:-${hostPlatform.system}}"
 
-  # Colors
-  R='\033[0;31m'
-  G='\033[0;32m'
-  Y='\033[1;33m'
-  C='\033[1;36m'
-  W='\033[0m'
+  # All the required functions
+  source ${./lib.sh}
 
-  # Derivate temporary paths
-  TMPDIR="''${NYX_TEMP:-''${TMPDIR}}"
-  NIX_BUILD_TOP="''${NYX_TEMP:-''${NIX_BUILD_TOP}}"
-  TMP="''${NYX_TEMP:-''${TMP}}"
-  TEMP="''${NYX_TEMP:-''${TEMP}}"
-  TEMPDIR="''${NYX_TEMP:-''${TEMPDIR}}"
-
-  # Create empty logs and artifacts
-  cd "$NYX_WD"
-  touch push.txt errors.txt success.txt failures.txt cached.txt upstream.txt eval-failures.txt
-  echo "{" > new-failures.nix
-
-  # Echo helpers
-  function echo_warning() {
-    echo -ne "''${Y}WARNING:''${W} "
-    echo "$@"
+  # Build jobs
+  function build-jobs() {
+    ${lib.strings.concatStringsSep "\n" packagesCmds}
   }
 
-  function echo_error() {
-    echo -ne "''${R}ERROR:''${W} " 1>&2
-    echo "$@" 1>&2
+  # Phases system
+  function default-phases () {
+    prepare
+    build-jobs
+    finish
+    deploy
   }
-
-  # Warn if we don't have automated cachix
-  if [ -z "$CACHIX_AUTH_TOKEN" ] && [ -z "$CACHIX_SIGNING_KEY" ]; then
-    echo_warning "No key for cachix -- building anyway."
-  fi
-
-  # Check if $1 is in the cache
-  function cached() {
-    set -e
-    ${Nix} path-info "$2" --store "$1" >/dev/null 2>/dev/null
-  }
-
-  # Creates list of what to build when only building what changed
-  if [ -n "''${NYX_CHANGED_ONLY:-}" ]; then
-    _DIFF=$(${Nix} build --no-link --print-out-paths --impure --expr "(builtins.getFlake \"$NYX_SOURCE\").devShells.$(uname -m)-linux.comparer.passthru.any \"$NYX_CHANGED_ONLY\"" || exit 13)
-
-    ln -s "$_DIFF" filter.txt
-  fi
-
-  # Helper to zip-merge _ALL_OUT_KEYS and _ALL_OUT_PATHS
-  function zip_path() {
-    for (( i=0; i<''${#_ALL_OUT_KEYS[*]}; ++i)); do
-      echo "''${_ALL_OUT_KEYS[$i]}" "''${_ALL_OUT_PATHS[$i]}"
-    done
-  }
-
-  # Per-derivation build function
-  function build() {
-    _WHAT="''${1:- アンノーン}"
-    _MAIN_OUT_PATH="''${2:-/dev/null}"
-    _FULL_TARGETS=("''${_ALL_OUT_KEYS[@]/#/$NYX_SOURCE\#_dev.packages.${hostPlatform.system}.}")
-    echo -n "* $_WHAT..."
-    # If NYX_CHANGED_ONLY is set, only build changed derivations
-    if [ -f filter.txt ] && ! ${Grep} -Pq "^$_WHAT\$" filter.txt; then
-      echo -e "''${Y} SKIP''${W}"
-      return 0
-    elif [ -z "''${NYX_REFRESH:-}" ] && cached 'https://chaotic-nyx.cachix.org' "$_MAIN_OUT_PATH"; then
-      echo "$_WHAT" >> cached.txt
-      echo -e "''${Y} CACHED''${W}"
-      zip_path >> full-pin.txt
-      return 0
-    elif cached 'https://cache.nixos.org' "$_MAIN_OUT_PATH"; then
-      echo "$_WHAT" >> upstream.txt
-      echo -e "''${Y} CACHED-UPSTREAM''${W}"
-      return 0
-    else
-      (while true; do echo -ne "''${C} BUILDING''${W}\n* $_WHAT..." && sleep 120; done) &
-      _KEEPALIVE=$!
-      if \
-        ( set -o pipefail;
-          ${Nix} build --json $NYX_FLAGS "''${_FULL_TARGETS[@]}" |\
-            ${Jq} -r '.[].outputs[]' \
-        ) 2>> errors.txt >> push.txt
-      then
-        echo "$_WHAT" >> success.txt
-        kill $_KEEPALIVE
-        echo -e "''${G} OK''${W}"
-        zip_path | tee -a to-pin.txt >> full-pin.txt
-        return 0
-      else
-        echo "$_WHAT" >> failures.txt
-        echo "  \"$_WHAT\" = \"$_MAIN_OUT_PATH\";" >> new-failures.nix
-        kill $_KEEPALIVE
-        echo -e "''${R} ERR''${W}"
-        return 1
-      fi
-    fi
-  }
-
-  # Main list of functions
-  ${lib.strings.concatStringsSep "\n" packagesCmds}
-
-  # Write EOF of the artifacts
-  echo "}" >> new-failures.nix
-
-  # Push logic
-  if [ -z "$CACHIX_AUTH_TOKEN" ] && [ -z "$CACHIX_SIGNING_KEY" ]; then
-    echo_error "No key for cachix -- failing to deploy."
-    exit 23
-  elif [ -n "''${NYX_RESYNC:-}" ] || [ -s push.txt ]; then
-    # Let nix digest store paths first
-    sleep 10
-
-    # Push all new deriations with compression
-    cat push.txt | ${Cachix} push chaotic-nyx \
-      --compression-method zstd
-
-    # Pin packages
-    if [ -e to-pin.txt ]; then
-      cat to-pin.txt | ${Xargs} -n 2 \
-        ${Cachix} -v pin chaotic-nyx
-    fi
-  else
-    echo_error "Nothing to push."
-    exit 42
-  fi
+  PHASES=''${PHASES:-default-phases};
+  for phase in $PHASES; do $phase; done
 
   # Useless exit but informative when running with "bash -x"
   exit 0
