97b200955c81c50aab5dac8d69325a8d07eec76c
PedroHLC ☭ <root@pedrohlc.com>
2023-10-07 14:49:26 -0300

    telegram-desktop_git: libprisma patch upstreamed

--- pkgs/telegram-desktop-git/default.nix
+++ pkgs/telegram-desktop-git/default.nix
@@ -26,14 +26,6 @@ gitOverride {
 
   postOverride = prevAttrs: {
     buildInputs = prevAttrs.buildInputs ++ (with final; [ kf6coreaddons_git ]);
-    postPatch = prevAttrs.postPatch + ''
-      (cd Telegram/ThirdParty/libprisma && \
-        patch -p1 < ${final.fetchpatch {
-          url = "https://github.com/desktop-app/libprisma/commit/b9a1ed1a1918b700eb3d140f5047f4f7533421c2.patch";
-          hash = "sha256-3mFQipw7ZH8Usj/38bnXtmVNaGuXrI4VRs8FQ7wbUoI=";
-        }} \
-      )
-    '';
     postFixup = ''
       qtWrapperArgs+=(
         --prefix LD_LIBRARY_PATH : "${final.glib_git.out}/lib"
