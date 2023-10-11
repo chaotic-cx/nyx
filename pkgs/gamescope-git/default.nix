c297fa923ae7ac648e7c64ee6aaebf7c9a22de5d
PedroHLC ☭ <root@pedrohlc.com>
2023-10-11 13:56:04 -0300

    gamescope_git: make it more prominent to failures

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -1,6 +1,16 @@
-{ prev, gitOverride, ... }:
+{ final, prev, gitOverride, ... }:
 
 gitOverride {
+  newInputs = with final; {
+    wlroots = wlroots_git;
+    wayland = wayland_git;
+    wayland-scanner = wayland-scanner_git;
+    wayland-protocols = wayland-protocols_git;
+    vulkan-loader = vulkanPackages_latest.vulkan-loader;
+    vulkan-headers = vulkanPackages_latest.vulkan-headers;
+    glslang = vulkanPackages_latest.glslang;
+  };
+
   nyxKey = "gamescope_git";
   prev = prev.gamescope;
 
@@ -13,7 +23,14 @@ gitOverride {
   };
   ref = "master";
 
-  postOverride = _prevAttrs: {
+  postOverride = prevAttrs: {
+    patches = prevAttrs.patches ++ [
+      (final.fetchpatch2 {
+        url = "https://github.com/ValveSoftware/gamescope/commit/d4ca57e1f4afe0b0798bf71406b1430a915a7bb3.patch";
+        hash = "sha256-XG8114bHGuhW7WmXufPMVf2yFKml8A4uTP3ucvIiH2I=";
+      })
+    ];
+
     # erase wlroots replacement since we're fetching submodules.
     postUnpack = "";
   };
