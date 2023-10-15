7b4af15686f1d0912087cec5ff1756b8c0903a50
PedroHLC ☭ <root@pedrohlc.com>
2023-10-15 16:45:43 -0300

    Revert everything except nixpkgs and linux-cachyos
    I wasn't home during this time and a lot of things broke.

--- maintenance/failures.nix
+++ maintenance/failures.nix
@@ -6,7 +6,9 @@
   "linuxPackages-hardened_cachyos.mba6x_bl" = "/nix/store/b8wmm80k8z521ynm7snjvzbsn12k7rnv-mba6x_bl-unstable-2017-12-30";
   "linuxPackages-hardened_cachyos.mbp2018-bridge-drv" = "/nix/store/wx87j69ygsrsdijsy56df676466k6h3v-mbp2018-bridge-drv-2020-01-31";
   "linuxPackages-hardened_cachyos.nvidia_dc" = "/nix/store/78lgk996k8wc0v1yz65zi9klyc7amk5f-nvidia-dc-520.61.05-6.5.6";
+  "linuxPackages-hardened_cachyos.nvidia_dc_520" = "/nix/store/78lgk996k8wc0v1yz65zi9klyc7amk5f-nvidia-dc-520.61.05-6.5.6";
   "linuxPackages-hardened_cachyos.openafs" = "/nix/store/s7xwkn5v4nq1gv9sw18br77j45fkgz86-openafs-1.8.10-6.5.6-cachyos";
+  "linuxPackages-hardened_cachyos.openafs_1_8" = "/nix/store/s7xwkn5v4nq1gv9sw18br77j45fkgz86-openafs-1.8.10-6.5.6-cachyos";
   "linuxPackages-hardened_cachyos.system76-acpi" = "/nix/store/397858xvjrgk0byaryfipxp7igzvzd5g-system76-acpi-module-1.0.2-6.5.6";
   "linuxPackages-hardened_cachyos.virtio_vmmci" = "/nix/store/wis256lnd74g8afr23pghmlz4r2cazn3-virtio_vmmci";
   "linuxPackages-hardened_cachyos.virtualboxGuestAdditions" = "/nix/store/870rss5j0zyp3hbnqkp4dzlprllhil62-VirtualBox-GuestAdditions-7.0.10-6.5.6";
@@ -18,7 +20,9 @@
   "linuxPackages_cachyos.mba6x_bl" = "/nix/store/p3ri5x7j586a1yc4qi0j7n37p153pn8k-mba6x_bl-unstable-2017-12-30";
   "linuxPackages_cachyos.mbp2018-bridge-drv" = "/nix/store/qzwnmk138v6sqglpc8vmy3kqn7yi1rwr-mbp2018-bridge-drv-2020-01-31";
   "linuxPackages_cachyos.nvidia_dc" = "/nix/store/zkkz91x0g4cm4j27q1g6khi9p8j84h9p-nvidia-dc-520.61.05-6.5.6";
+  "linuxPackages_cachyos.nvidia_dc_520" = "/nix/store/zkkz91x0g4cm4j27q1g6khi9p8j84h9p-nvidia-dc-520.61.05-6.5.6";
   "linuxPackages_cachyos.openafs" = "/nix/store/hv5bnmgf21mgvdy7fz1ccy5w1j27i1gz-openafs-1.8.10-6.5.6-cachyos";
+  "linuxPackages_cachyos.openafs_1_8" = "/nix/store/hv5bnmgf21mgvdy7fz1ccy5w1j27i1gz-openafs-1.8.10-6.5.6-cachyos";
   "linuxPackages_cachyos.system76-acpi" = "/nix/store/32g0xpz8lcv6y37gadqcq7m72ix8xp64-system76-acpi-module-1.0.2-6.5.6";
   "linuxPackages_cachyos.virtio_vmmci" = "/nix/store/3nr8shidqih4g0nxzy97dszzh22fg4gg-virtio_vmmci";
   "linuxPackages_cachyos.virtualboxGuestAdditions" = "/nix/store/qd9jf9f9rqw91y5dc4krp3b1gq11d5sl-VirtualBox-GuestAdditions-7.0.10-6.5.6";
