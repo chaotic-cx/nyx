68bfaaf7c380f369baf8c4d09cc4019797c56a68
PedroHLC ☭ <root@pedrohlc.com>
2023-11-20 12:35:59 -0300

    glib_git: 20231010212346-2f5088b -> 20231114154630-09b728b

--- pkgs/glib-git/default.nix
+++ pkgs/glib-git/default.nix
@@ -16,11 +16,6 @@ gitOverride {
   postOverride = prevAttrs: {
     patches =
       (nyxUtils.removeByBaseName "split-dev-programs.patch" prevAttrs.patches)
-      ++ [
-        (final.fetchpatch {
-          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/8f5cf46a9b75b934323971b8bc9dc571888164f0/pkgs/development/libraries/glib/split-dev-programs.patch";
-          hash = "sha256-fPGXPBPMNdpwZn+VJMRTzDK2UX/0q09RAyigbIf2jq4=";
-        })
-      ];
+      ++ [ ./split-dev-programs.patch ];
   };
 }
