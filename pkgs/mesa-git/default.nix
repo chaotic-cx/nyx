ca127ae29e88b7ca44d2f6e9667ea930e98d141e
PedroHLC ☭ <root@pedrohlc.com>
2023-11-21 08:37:23 -0300

    mesa_git: 20231114160458-2f1cb79 -> 20231121090826-aba00ff

--- pkgs/mesa-git/default.nix
+++ pkgs/mesa-git/default.nix
@@ -23,21 +23,9 @@ gitOverride (current: {
   version = builtins.substring 0 (builtins.stringLength prev.mesa.version) current.rev;
 
   postOverride = prevAttrs: {
-    buildInputs = prevAttrs.buildInputs ++ (with final; [ libunwind lm_sensors ]);
-    mesonFlags =
-      builtins.map
-        (builtins.replaceStrings [ "virtio-experimental" ] [ "virtio" ])
-        prevAttrs.mesonFlags;
     patches =
-      (nyxUtils.removeByBaseName
-        "disk_cache-include-dri-driver-path-in-cache-key.patch"
-        (nyxUtils.removeByBaseName
-          "opencl.patch"
-          prevAttrs.patches
-        )
-      ) ++ [
-        ./opencl.patch
-        ./disk_cache-include-dri-driver-path-in-cache-key.patch
+      prevAttrs.patches
+       ++ [
         ./gbm-backend.patch
       ];
     # expose gbm backend and rename vendor (if necessary)
@@ -45,15 +33,18 @@ gitOverride (current: {
       if gbmDriver
       then prevAttrs.outputs ++ [ "gbm" ]
       else prevAttrs.outputs;
+    # allow renaming the new backend name
     postPatch =
       if gbmBackend != "dri_git" then prevAttrs.postPatch + ''
         sed -i"" 's/"dri_git"/"${gbmBackend}"/' src/gbm/backends/dri/gbm_dri.c src/gbm/main/backend.c
       '' else prevAttrs.postPatch;
+    # move new backend to its own output (if necessary)
     postInstall =
       if gbmDriver then prevAttrs.postInstall + ''
         mkdir -p $gbm/lib/gbm
         ln -s $out/lib/libgbm.so $gbm/lib/gbm/${gbmBackend}_gbm.so
       '' else prevAttrs.postInstall;
+    # test and accessible information
     passthru = prevAttrs.passthru // {
       inherit gbmBackend;
       tests.smoke-test = import ./test.nix
