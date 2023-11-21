53e11a5c5a2c8456b418c928814f287d1229ebda
PedroHLC ☭ <root@pedrohlc.com>
2023-11-21 08:58:52 -0300

    Revert "glib_git: 20231010212346-2f5088b -> 20231114154630-09b728b"
    This reverts commit bbee04eafe862a5ae181f50bbde6d8f4e20587fd.

--- pkgs/glib-git/default.nix
+++ pkgs/glib-git/default.nix
@@ -16,6 +16,11 @@ gitOverride {
   postOverride = prevAttrs: {
     patches =
       (nyxUtils.removeByBaseName "split-dev-programs.patch" prevAttrs.patches)
-      ++ [ ./split-dev-programs.patch ];
+      ++ [
+        (final.fetchpatch {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/8f5cf46a9b75b934323971b8bc9dc571888164f0/pkgs/development/libraries/glib/split-dev-programs.patch";
+          hash = "sha256-fPGXPBPMNdpwZn+VJMRTzDK2UX/0q09RAyigbIf2jq4=";
+        })
+      ];
   };
 }
