b6c1367c67297b6620319a81ebee60e8df5e9193
PedroHLC ☭ <root@pedrohlc.com>
2023-12-21 10:03:39 -0300

    scx: 0.1.1.r85.g8ea5850 -> 0.1.2

--- pkgs/scx/common.nix
+++ pkgs/scx/common.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
-{
-  version = "0.1.1.r85.g8ea5850";
+rec {
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "sched-ext";
     repo = "scx";
-    rev = "8ea585096722c9433b0663cf2987744b88caa311";
-    hash = "sha256-S5d2R3FOO+lgWtm2qpzV479pnjwKYiGBRJwyt3CsGsU=";
+    rev = "v${version}";
+    hash = "sha256-yY1HQUz9qJqHRjV393Pa4oILakCj0w3IU3jpFh3uzQ0=";
     fetchSubmodules = true;
   };
 
