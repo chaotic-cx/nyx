0ad4beb01575a8e867daca59c66e2e0a8a1702ad
PedroHLC ☭ <root@pedrohlc.com>
2024-01-10 11:35:12 -0300

    mesa_git: use libdrm_git again

--- pkgs/mesa-git/default.nix
+++ pkgs/mesa-git/default.nix
@@ -25,9 +25,11 @@ gitOverride (current: {
     if is32bit then with final64; {
       meson = meson32_1_3;
       directx-headers = directx-headers32_1_611;
+      libdrm = libdrm32_git;
     } else with final; {
       meson = meson_1_3;
       directx-headers = directx-headers_1_611;
+      libdrm = libdrm_git;
       # We need to mention those besides "all", because of the usage of nix's `lib.elem` in
       # the original derivation.
       galliumDrivers = [ "all" "zink" "d3d12" ];
