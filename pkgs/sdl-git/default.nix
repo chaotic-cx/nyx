760852a585a2e4ee4caeb5008d43ca91dd48fdb5
PedroHLC ☭ <root@pedrohlc.com>
2024-01-10 10:44:57 -0300

    sdl_git: init at 20240109194312-6407e0c

---
+++ pkgs/sdl-git/default.nix
@@ -0,0 +1,25 @@
+{ final, prev, gitOverride, ... }:
+
+gitOverride {
+  nyxKey = "sdl_git";
+  prev = prev.SDL2;
+
+  versionNyxPath = "pkgs/sdl-git/version.json";
+  fetcher = "fetchFromGitHub";
+  fetcherData = {
+    owner = "libsdl-org";
+    repo = "SDL";
+  };
+  ref = "main";
+
+  postOverride = prevAttrs: {
+    patches = [];
+    postPatch = ''
+      substituteInPlace cmake/sdl3.pc.in \
+        --replace 'libdir=''${prefix}/' 'libdir=' \
+        --replace 'includedir=''${prefix}/' 'includedir=' \
+        --replace 'exec_prefix=''${prefix}' '@CMAKE_INSTALL_BINDIR@'
+    '';
+    nativeBuildInputs = with final; [ cmake ] ++ prevAttrs.nativeBuildInputs;
+  };
+}
