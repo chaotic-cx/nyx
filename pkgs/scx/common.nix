7bf8c1a486fa7c8603a077203efa2255cc9b0aff
Pedro Lara Campos <root@pedrohlc.com>
2024-01-16 15:07:31 -0300

    scx: 0.1.4 -> 0.1.5 (#549)

--- pkgs/scx/common.nix
+++ pkgs/scx/common.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
 rec {
-  version = "0.1.4";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "sched-ext";
     repo = "scx";
     rev = "v${version}";
-    hash = "sha256-nGsLb5+1ONKuQsX4YylTpUo/C8ZNgwEC2K3QvWixMng=";
+    hash = "sha256-frPPzR9ejbqfKc8cRrkVYw595jo/l8k3F75ekQDPsmc=";
     fetchSubmodules = true;
   };
 
