c07840fa65524f8fbe7df0246a45ab11849a69fe
PedroHLC ☭ <root@pedrohlc.com>
2024-01-23 10:28:20 -0300

    openvr_git: init at 20240119204020-f51d87e

---
+++ pkgs/openvr-git/default.nix
@@ -0,0 +1,17 @@
+{ final, prev, gitOverride, ... }:
+
+gitOverride {
+  nyxKey = "openvr_git";
+  prev = prev.openvr;
+
+  versionNyxPath = "pkgs/openvr-git/version.json";
+  fetcher = "fetchFromGitHub";
+  fetcherData = {
+    owner = "ValveSoftware";
+    repo = "openvr";
+    fetchSubmodules = true;
+  };
+  ref = "master";
+
+  postOverride = prevAttrs: {};
+}
