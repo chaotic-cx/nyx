922599636f417bd3400e9f382381e2f8f685ecd9
Pedro Lara Campos <root@pedrohlc.com>
2024-02-03 12:06:46 -0300

    third-party: init conduit_git, jujutsu_git & niri_git (#578)

--- flake.nix
+++ flake.nix
@@ -2,23 +2,75 @@ rec {
   description = "Nix flake for \"too much bleeding-edge\" and unreleased packages (e.g., mesa_git, linux_cachyos, firefox_nightly, sway_git, gamescope_git). And experimental modules (e.g., HDR, duckdns).";
 
   inputs = {
-    compare-to.url = "https://flakehub.com/f/chaotic-cx/nix-empty-flake/0.1.2.tar.gz"; # pinned, used when comparing changes between commits
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.1.tar.gz"; # pinned, used by "schemas" output
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    compare-to.url = "https://flakehub.com/f/chaotic-cx/nix-empty-flake/=0.1.2.tar.gz"; # pinned, used when comparing changes between commits
+    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/=0.1.1.tar.gz"; # pinned, used by "schemas" output
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
     home-manager = {
-      url = "https://flakehub.com/f/nix-community/home-manager/0.1.0.tar.gz";
+      url = "https://flakehub.com/f/nix-community/home-manager/0.1.*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     systems.url = "github:nix-systems/default-linux";
     yafas = {
-      url = "https://flakehub.com/f/UbiqueLambda/yafas/0.1.0.tar.gz";
+      url = "https://flakehub.com/f/UbiqueLambda/yafas/0.1.*.tar.gz";
       inputs.systems.follows = "systems";
       inputs.flake-schemas.follows = "flake-schemas";
+    };
+
+    # thirdy-party repositories
+    conduit = {
+      url = "gitlab:famedly/conduit/next";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.attic.follows = "attic";
+      inputs.crane.follows = "crane";
+      inputs.fenix.follows = "fenix";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nix-compat.follows = "nix-compat";
+      inputs.nix-filter.follows = "nix-filter";
     };
     jovian = {
       url = "github:Jovian-Experiments/Jovian-NixOS";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    jujutsu = {
+      url = "github:martinvonz/jj";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    niri = {
+      url = "github:YaLTeR/niri";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.crane.follows = "crane";
+      inputs.fenix.follows = "fenix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nix-filter.follows = "nix-filter";
+    };
+
+    # thirdy-party repositories' common dependencies
+    attic = {
+      url = "https://flakehub.com/f/zhaofengli/attic/0.1.*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.crane.follows = "crane";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    crane = {
+      url = "https://flakehub.com/f/ipetkov/crane/*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "https://flakehub.com/f/edolstra/flake-compat/*.tar.gz";
+      flake = false;
+    };
+    flake-utils = {
+      url = "https://flakehub.com/f/numtide/flake-utils/0.1.*.tar.gz";
+      inputs.systems.follows = "systems";
+    };
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1.*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs = { nixpkgs, yafas, ... }@inputs: yafas.withAllSystems nixpkgs
