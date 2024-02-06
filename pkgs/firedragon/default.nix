f2b3ec52fa959678915e685b4a890f0d410f1264
Nico <github@dr460nf1r3.org>
2024-02-06 15:05:44 +0100

    firedragon: 119.0 -> 11.9.0 (#581)

--- pkgs/firedragon/default.nix
+++ pkgs/firedragon/default.nix
@@ -6,23 +6,25 @@
 let
   firedragon-src = callPackage ./firedragon.nix { };
 in
-(buildMozillaMach rec {
-  pname = "firedragon";
+((buildMozillaMach rec {
   applicationName = "FireDragon";
   binaryName = "firedragon";
-  version = firedragon-src.packageVersion;
-  src = firedragon-src.firefox;
-  inherit (firedragon-src) extraConfigureFlags extraPatches extraPostPatch extraPassthru;
+  pname = "firedragon";
+  src = firedragon-src.floorp;
+  inherit (firedragon-src) extraConfigureFlags extraPatches extraPostPatch extraPassthru packageVersion;
+
+  # Must match the contents of `browser/config/version.txt` in the source tree
+  version = "115.7.0";
 
   updateScript = callPackage ./update.nix { };
 
   meta = {
     badPlatforms = lib.platforms.darwin;
-    broken = stdenv.buildPlatform.is32bit; # since Firefox 60, build on 32-bit platforms fails with "out of memory".
-    description = "A fork of LibreWolf, focused on being easier to use";
+    description = "Floorp fork build using custom branding & settings";
     homepage = "https://github.com/dr460nf1r3/firedragon-browser";
     license = lib.licenses.mpl20;
     maintainers = with lib; [ maintainers.dr460nf1r3 ];
+    broken = stdenv.buildPlatform.is32bit; # since Firefox 60, build on 32-bit platforms fails with "out of memory".
     maxSilent = 14400; # 4h, double the default of 7200s (c.f. #129212, #129115)
     platforms = lib.platforms.unix;
     mainProgram = "firedragon";
@@ -30,5 +32,15 @@ in
 }).override {
   crashreporterSupport = false;
   enableOfficialBranding = false;
-  pgoSupport = false; # Profiling gets stuck and doesn't terminate.
+  pgoSupport = true;
+  privacySupport = true;
+  webrtcSupport = true;
+}).overrideAttrs {
+  MOZ_APP_REMOTINGNAME = "firedragon";
+  MOZ_CRASHREPORTER = "";
+  MOZ_DATA_REPORTING = "";
+  MOZ_REQUIRE_SIGNING = "";
+  MOZ_SERVICES_HEALTHREPORT = "";
+  MOZ_TELEMETRY_REPORTING = "";
+  RUSTC_OPT_LEVEL = "2";
 }
--- pkgs/firedragon/firedragon.nix
+++ pkgs/firedragon/firedragon.nix
@@ -3,13 +3,27 @@ let
   src = callPackage ./src.nix { };
 in
 rec {
-  inherit (src) packageVersion firefox source source-firedragon;
+  inherit (src) packageVersion floorp firedragon-common firedragon-settings;
 
   extraPatches = [ ];
 
   extraConfigureFlags = [
     "--allow-addon-sideload"
+    "--disable-crashreporter"
     "--disable-debug"
+    "--disable-debug-js-modules"
+    "--disable-debug-symbols"
+    "--disable-default-browser-agent"
+    "--disable-gpsd"
+    "--disable-necko-wifi"
+    "--disable-rust-tests"
+    "--disable-tests"
+    "--disable-updater"
+    "--disable-warnings-as-errors"
+    "--disable-webspeech"
+    "--enable-bundled-fonts"
+    "--enable-jxl"
+    "--enable-proxy-bypass-protection"
     "--with-app-basename=FireDragon"
     "--with-app-name=firedragon"
     "--with-branding=browser/branding/firedragon"
@@ -17,32 +31,33 @@ rec {
     "--with-unsigned-addon-scopes=app,system"
   ];
 
-  # FireDragon builds on top of LibreWolf, therefore we can use mostly the same patches
   extraPostPatch = ''
-    mkdir -p temp
-    cp ${source}/assets/patches.txt temp
-    sed -i 's&patches/librewolf-pref-pane.patch&&g' temp/patches.txt
-    sed -i 's&patches/ui-patches/privacy-preferences.patch&&g' temp/patches.txt
-    sed -i '/^$/d' temp/patches.txt
+    cp -r "${firedragon-common}/source_files/browser" .
+    patch -p1 < ${firedragon-common}/patches/floorp/allow-ubo-private-mode.patch
+    patch -p1 < ${firedragon-common}/patches/floorp/custom-ubo-assets-bootstrap-location.patch
+    patch -p1 < ${firedragon-common}/patches/floorp/hide-passwordmgr.patch
+    patch -p1 < ${firedragon-common}/patches/floorp/remove_addons.patch
+    patch -p1 < ${firedragon-common}/patches/floorp/sed-patches/stop-undesired-requests.patch
+    patch -p1 < ${firedragon-common}/patches/floorp/urlbarprovider-interventions.patch
 
-    while read patch_name; do
-      echo "applying LibreWolf patch: $patch_name"
-      patch -p1 < ${source}/$patch_name
-    done <temp/patches.txt
+    patch -p1 < ${firedragon-common}/patches/pref-pane/pref-pane-small.patch
+    cp "${firedragon-common}/patches/pref-pane/category-firedragon.svg" browser/themes/shared/preferences/category-firedragon.svg
+    cp "${firedragon-common}/patches/pref-pane/firedragon.css" browser/themes/shared/preferences/firedragon.css
+    cp "${firedragon-common}/patches/pref-pane/firedragon.inc.xhtml" browser/components/preferences/firedragon.inc.xhtml
+    cp "${firedragon-common}/patches/pref-pane/firedragon.js" browser/components/preferences/firedragon.js
+    cat < "${firedragon-common}/patches/pref-pane/preferences.ftl" >> browser/locales/en-US/browser/preferences/preferences.ftl
 
-    cp -r "${source-firedragon}/common/source_files/browser" .
-    patch -p1 < ${source-firedragon}/common/patches/custom/add_firedragon_svg.patch
-    patch -p1 < ${source-firedragon}/common/patches/custom/librewolf-pref-pane.patch
-    patch -p1 < ${source-firedragon}/common/patches/custom/privacy-preferences.patch
-
-    cp ${source}/assets/search-config.json services/settings/dumps/main/search-config.json
+    sed -i '/MOZ_CRASHREPORTER/ s/True/False/' browser/moz.configure
+    sed -i '/MOZ_DATA_REPORTING/ s/True/False/' browser/moz.configure
     sed -i '/MOZ_NORMANDY/ s/True/False/' browser/moz.configure
+    sed -i '/MOZ_REQUIRE_SIGNING/ s/True/False/' browser/moz.configure
     sed -i '/MOZ_SERVICES_HEALTHREPORT/ s/True/False/' browser/moz.configure
+    sed -i '/MOZ_TELEMETRY_REPORTING/ s/True/False/' browser/moz.configure
   '';
 
-  extraPrefsFiles = [ "${source-firedragon}/firedragon.cfg" ];
+  extraPrefsFiles = [ "${firedragon-settings}/firedragon.cfg" ];
 
-  extraPoliciesFiles = [ "${source-firedragon}/distribution/policies.json" ];
+  extraPoliciesFiles = [ "${firedragon-settings}/distribution/policies.json" ];
 
   extraPassthru = {
     firedragon = { inherit src extraPatches; };
