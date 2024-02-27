b2de12d22bedc13d12d92dce87a193748977604d
PedroHLC ☭ <root@pedrohlc.com>
2024-02-27 10:38:09 -0300

    nixos/scx: init

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,6 +8,7 @@ let
     nordvpn = import ./nordvpn.nix;
     nyx-cache = import ./nyx-cache.nix fromFlakes;
     nyx-overlay = import ../common/nyx-overlay.nix fromFlakes;
+    scx = import ./scx.nix;
     steam-compat-tools = import ./steam-compat-tools.nix;
     qtile = import ./qtile.nix;
     zfs-impermanence-on-shutdown = import ./zfs-impermanence-on-shutdown.nix;
---
+++ modules/nixos/scx.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, config, ... }:
+# Originally from https://github.com/nix-community/nur-combined/blob/1b7e474178abfdeb6f89142d37fcc4854a16a5a1/repos/oluceps/modules/scx.nix
+with lib;
+let
+  cfg = config.chaotic.scx;
+in
+{
+  options.chaotic.scx = {
+    enable = mkEnableOption "scx service";
+    package = mkPackageOptionMD pkgs "scx" { };
+    scheduler = mkOption {
+      type = types.str;
+      default = "scx_rustland";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.scx ];
+
+    systemd.services.scx = {
+      wantedBy = [ "multi-user.target" ];
+      description = "scheduler daemon";
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+        ExecStart = "${lib.getExe' cfg.package cfg.scheduler}";
+        Restart = "on-failure";
+      };
+    };
+  };
+}
