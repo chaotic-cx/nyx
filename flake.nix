64ae864a93ff9c95a87d36d4aa36cd315ff046c8
June <june@girlboss.ceo>
2024-03-11 20:13:20 -0400

    conduwuit_git: switch remote to github (#626)
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@ rec {
 
     # thirdy-party repositories
     conduit = {
-      url = "gitlab:girlbossceo/conduwuit";
+      url = "github:girlbossceo/conduwuit";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.attic.follows = "attic";
       inputs.crane.follows = "crane";
