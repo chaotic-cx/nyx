b2f48618cbbefb126525000197d07758b27e4f38
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-24 18:54:57 +0530

    Fix bug

--- overlays/default.nix
+++ overlays/default.nix
@@ -50,6 +50,7 @@ let
 
   # Too much variations
   cachyosPackages = callOverride ../pkgs/linux-cachyos/all-packages.nix { };
+  stdenvLLVM = final.callPackage ./stdenv-llvm.nix { };
 
   # Microarch stuff
   makeMicroarch = lvl: with final;
@@ -181,10 +182,10 @@ in
   linuxPackages_cachyos = cachyosPackages.cachyos;
   linuxPackages_cachyos-hardened = cachyosPackages.cachyos-hardened;
   linuxPackages_cachyos-lto = cachyosPackages.cachyos-lto.extend (
-    lto_final: lto_prev: with linuxKernel.packages.linux_cachyos-lto; {
-      virtualbox = lto_prev.virtualbox.overrideAttrs (_prevattrs: { stdenv = pkgs.clangStdenv; });
-      zenpower = lto_prev.zenpower.overrideAttrs (_prevattrs: { stdenv = pkgs.clangStdenv; });
-      zfs = lto_prev.zfs.overrideAttrs (_prevattrs: { stdenv = pkgs.clangStdenv; });
+    _lto_final: lto_prev: {
+      virtualbox = lto_prev.virtualbox.overrideAttrs (_prevattrs: { stdenv = stdenvLLVM; });
+      zenpower = lto_prev.zenpower.overrideAttrs (_prevattrs: { stdenv = stdenvLLVM; });
+      zfs = lto_prev.zfs.overrideAttrs (_prevattrs: { stdenv = stdenvLLVM; });
     }
   );
   linuxPackages_cachyos-sched-ext = cachyosPackages.cachyos-sched-ext;
