fcca2ca3ceee4caa84822f2697e9b6856fc5dbda
PedroHLC <root@pedrohlc.com>
2024-04-01 22:34:50 -0300

    gamescope_git: 20240326033905-e0098f5 -> 20240331040115-bc0cf62

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -3,6 +3,15 @@
 gitOverride {
   newInputs = with final; {
     openvr = openvr_git;
+    wlroots = wlroots_git.overrideAttrs (_wlrPrev: {
+      src = fetchFromGitLab {
+        domain = "gitlab.freedesktop.org";
+        owner = "wlroots";
+        repo = "wlroots";
+        rev = "a5c9826e6d7d8b504b07d1c02425e6f62b020791";
+        hash = "sha256-G7CvsSRryNCoknWhfoZvkf33967xI51WkemA6ms3vo4=";
+      };
+    });
   };
 
   nyxKey = if isWSI then "gamescope-wsi_git" else "gamescope_git";
@@ -21,11 +30,6 @@ gitOverride {
   postOverride = prevAttrs: {
     buildInputs = with final; [ seatd xwayland libdecor ] ++ (with xorg; [ xcbutilwm xcbutilerrors ]) ++ prevAttrs.buildInputs;
 
-    patches = with final; prevAttrs.patches ++ [
-      (fetchpatch2 {
-        url = "https://github.com/ValveSoftware/gamescope/commit/85432af61b779a02b636fdc29d98aba5e89fcff7.patch";
-        hash = "sha256-XmrozomRB0adizdZPH9yK9SOA02XrABl6DWciyNNQn0=";
-      })
-    ];
+    patches = with final; prevAttrs.patches ++ [ ./6.8-color.patch ];
   };
 }
