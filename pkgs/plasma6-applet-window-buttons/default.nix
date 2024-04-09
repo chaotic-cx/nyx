7c5909c7da7a7a62a88fc6b682ba746a79d6388c
TNE <tne@garudalinux.org>
2024-04-09 17:11:02 +0200

    plasma6-applet-window-buttons: fix packaging mistake

--- pkgs/plasma6-applet-window-buttons/default.nix
+++ pkgs/plasma6-applet-window-buttons/default.nix
@@ -1,11 +1,13 @@
 { prev, ... }:
 let
-  patch-plasma6 = prev.fetchpatch {
-    url = "https://github.com/psifidotos/applet-window-buttons/compare/master...moodyhunter:326382805641d340c9902689b549e4488682f553.patch";
+  patch-plasma6 = prev.fetchurl {
+    url = "https://github.com/psifidotos/applet-window-buttons/compare/90e37501871a7797e2befc4c524e56f320170780...moodyhunter:326382805641d340c9902689b549e4488682f553.patch";
     hash = "sha256-m9ePAVoOYDOHoSAU9/kyI4IAyMAkNIBsela7RWnwFuw=";
   };
 in
 prev.libsForQt5.applet-window-buttons.overrideAttrs (previousAttrs: {
+  pname = "plasma6-applet-window-buttons";
+
   patches = [ patch-plasma6 ];
 
   nativeBuildInputs = with prev; with prev.kdePackages; [
@@ -22,4 +24,6 @@ prev.libsForQt5.applet-window-buttons.overrideAttrs (previousAttrs: {
   ];
 
   dontWrapQtApps = true;
+
+  meta.description = "Plasma 6 applet in order to show window buttons in your panels";
 })
