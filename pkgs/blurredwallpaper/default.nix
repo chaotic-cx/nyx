f9ed9412d42964f8e71d01f9143a3303a3ea20a0
TNE <38938720+JustTNE@users.noreply.github.com>
2024-04-09 15:49:02 +0200

    blurredwallpaper: 3.0.0 -> 3.0.1

--- pkgs/blurredwallpaper/default.nix
+++ pkgs/blurredwallpaper/default.nix
@@ -4,22 +4,21 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "blurredwallpaper";
-  version = "3.0.0";
+  version = "3.0.1";
 
   src = fetchFromGitHub {
     owner = "bouteillerAlan";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-mCqf03cXlTl4vuJIzVEiYhHQsAPG3i6yHQ3Xdt06HfA=";
+    hash = "sha256-zPnFk/FImem/x/ocA8KfVuh0bZE/C75dISrhb1AcHm8=";
   };
 
   propagatedBuildInputs = [ ];
 
   installPhase = ''
     runHook preInstall
-    install -d $out/share/plasma/wallpapers/a2n.blur
-    cp -r * $out/share/plasma/wallpapers/a2n.blur
-    rm $out/share/plasma/wallpapers/a2n.blur/{AUTHORS,CHANGELOG,CODE_OF_CONDUCT.md,CONTRIBUTING.md,LICENSE,README.md,SECURITY.md}
+    install -d $out/share/plasma/wallpapers/a2n.blur{,.plasma5}
+    cp -r a2n.blur{,.plasma5} $out/share/plasma/wallpapers/
     runHook postInstall
   '';
 
