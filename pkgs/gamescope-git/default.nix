eb64bf267342ecc37f49c07f9e98c4ac063d2e65
Anthony Rabbito <hello@anthonyrabbito.com>
2024-04-11 20:07:03 -0400

    fix(gamescope): failing build (#676)
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- pkgs/gamescope-git/default.nix
+++ pkgs/gamescope-git/default.nix
@@ -4,9 +4,8 @@ gitOverride (current: {
   newInputs = with final; {
     openvr = openvr_git;
     wlroots = wlroots_git.overrideAttrs (_wlrPrev: {
-      src = fetchFromGitLab {
-        domain = "gitlab.freedesktop.org";
-        owner = "wlroots";
+      src = fetchFromGitHub {
+        owner = "Joshua-Ashton";
         repo = "wlroots";
         rev = "a5c9826e6d7d8b504b07d1c02425e6f62b020791";
         hash = "sha256-G7CvsSRryNCoknWhfoZvkf33967xI51WkemA6ms3vo4=";
@@ -29,8 +28,6 @@ gitOverride (current: {
 
   postOverride = prevAttrs: {
     buildInputs = with final; [ seatd xwayland libdecor ] ++ (with xorg; [ xcbutilwm xcbutilerrors ]) ++ prevAttrs.buildInputs;
-
-    patches = with final; prevAttrs.patches ++ [ ./6.8-color.patch ];
 
     postPatch =
       let shortRev = nyxUtils.shorter current.rev; in
