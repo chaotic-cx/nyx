cfc865999c1d60fae12ad38c744eaf69ec43dc56
Nιƈσ 🐉🔥 <github@dr460nf1r3.org>
2024-05-20 19:00:38 +0200

    firedragon: fix update script (again) (#729)

--- pkgs/firedragon/update.nix
+++ pkgs/firedragon/update.nix
@@ -29,7 +29,7 @@ writeShellScript "update-firedragon" ''
   PATH=${path}
 
   srcJson=pkgs/firedragon/version.json
-  localVer=$(jq -r .versison <$srcJson)
+  localVer=$(jq -r .version <$srcJson)
 
   latestVer=$(curl -s https://gitlab.com/api/v4/projects/55893651/releases/ | jq '.[0].tag_name' -r | sed 's/v//g')
 
@@ -37,7 +37,7 @@ writeShellScript "update-firedragon" ''
     exit 0
   fi
 
-  latestSha256=$(nix-prefetch-url --type sha256 "https://gitlab.com/api/v4/projects/55893651/packages/generic/firedragon/${latestVer}/firedragon-v${latestVer}.source.tar.zst")
+  latestSha256=$(nix-prefetch-url --type sha256 "https://gitlab.com/api/v4/projects/55893651/packages/generic/firedragon/$latestVer/firedragon-v$latestVer.source.tar.zst")
   latestHash=$(nix-hash --to-sri --type sha256 "$latestSha256")
 
   jq \
