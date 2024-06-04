87d7633062323ce1afee54cbe890adc95e43eb5e
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-06-04 22:31:38 +0530

    nixos/scx: enable stricter check by hardcoding possible schedulers
    Currently we can put any value like "hello-world" there and there is no way to check if that is appropiate.
    This checking allows correct values to be used and warns in case of mistake.
    This should check for typo errors like "scx-rustland" instead of "scx_rustland" which does not start the service properly.

--- modules/nixos/scx.nix
+++ modules/nixos/scx.nix
@@ -1,15 +1,27 @@
 { lib, pkgs, config, ... }:
 # Originally from https://github.com/nix-community/nur-combined/blob/1b7e474178abfdeb6f89142d37fcc4854a16a5a1/repos/oluceps/modules/scx.nix
-with lib;
 let
   cfg = config.chaotic.scx;
 in
 {
   options.chaotic.scx = {
-    enable = mkEnableOption "scx service";
-    package = mkPackageOptionMD pkgs "scx" { };
-    scheduler = mkOption {
-      type = types.str;
+    enable = lib.mkEnableOption "scx service";
+    package = lib.mkPackageOptionMD pkgs "scx" { };
+    scheduler = lib.mkOption {
+      type = lib.types.enum [
+        "scx_central"
+        "scx_flatcg"
+        "scx_lavd"
+        "scx_layered"
+        "scx_nest"
+        "scx_pair"
+        "scx_qmap"
+        "scx_rlfifo"
+        "scx_rustland"
+        "scx_rusty"
+        "scx_simple"
+        "scx_userland"
+      ];
       default = "scx_rustland";
       example = "scx_rusty";
       description = ''
@@ -18,7 +30,7 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = [ cfg.package ];
 
     systemd.services.scx = {
