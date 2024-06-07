d1376e1cb04fdf3aeb465738c1b7c52277e5bff9
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-06-07 17:49:15 +0530

    nixos/hdr: remove hdr.kernelPackages option
    From now on, a kernel built with AMD_PRIVATE_COLOR should be used instead

--- modules/nixos/hdr.nix
+++ modules/nixos/hdr.nix
@@ -3,7 +3,6 @@ let
   cfg = config.chaotic.hdr;
 
   configuration = strength: {
-    boot.kernelPackages = strength cfg.kernelPackages;
     programs.steam.gamescopeSession.enable = true; # HDR can only be used with headless Gamescope right now...
     programs.gamescope = {
       args = [ "--hdr-enabled" ];
@@ -38,16 +37,6 @@ in
           Isolates the changes in a specialisation.
         '';
       };
-    kernelPackages =
-      lib.mkOption {
-        default = pkgs.linuxPackages_cachyos;
-        defaultText = lib.literalExpression "pkgs.linuxPackages_cachyos";
-        example = lib.literalExpression "pkgs.linuxKernel.packages.linux_hdr";
-        type = lib.types.raw;
-        description = ''
-          Kernel+packages with "AMD Color Management" patches applied.
-        '';
-      };
     wsiPackage =
       lib.mkOption {
         default = pkgs.gamescope-wsi;
@@ -60,4 +49,11 @@ in
       };
   };
   config = lib.mkIf cfg.enable (lib.mkMerge [ sysConfig specConfig ]);
+
+  imports = [
+    (lib.mkRemovedOptionModule
+      [ "chaotic" "hdr" "kernelPackages" ]
+      "kernelPackages option is deprecated. Please use a kernel built with the `AMD_PRIVATE_COLOR` flag."
+    )
+  ];
 }
