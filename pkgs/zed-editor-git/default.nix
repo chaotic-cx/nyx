d23de196d22f564be8f02ac366a1e3919abacb12
PedroHLC <root@pedrohlc.com>
2024-06-27 08:47:49 -0300

    zed-editor_git: 20240601013615-94c3101 -> 0.141.2

--- pkgs/zed-editor-git/default.nix
+++ pkgs/zed-editor-git/default.nix
@@ -9,6 +9,7 @@ gitOverride {
   fetcherData = {
     owner = "zed-industries";
     repo = "zed";
+    fetchSubmodules = true;
   };
   ref = "main";
 
@@ -18,9 +19,22 @@ gitOverride {
   };
 
   postOverride = prevAttrs: {
-    postPatch = (prevAttrs.postPatch or "") + ''
-      sed -i"" 's+zed::node_binary_path()?+"${final.nodejs_18}"+g' extensions/*/src/*.rs
-    '';
     env = prevAttrs.env // { OPENSSL_NO_VENDOR = 1; };
+    # pull ahead nixpkgs#317564
+    nativeBuildInputs = with final; [ clang ] ++ prevAttrs.nativeBuildInputs;
+    postInstall = ''
+      install -D ${prevAttrs.src}/crates/zed/resources/app-icon@2x.png $out/share/icons/hicolor/1024x1024@2x/apps/zed.png
+      install -D ${prevAttrs.src}/crates/zed/resources/app-icon.png $out/share/icons/hicolor/512x512/apps/zed.png
+
+      # extracted from https://github.com/zed-industries/zed/blob/v0.141.2/script/bundle-linux
+      (
+        export DO_STARTUP_NOTIFY="true"
+        export APP_CLI="zed"
+        export APP_ICON="zed"
+        export APP_NAME="Zed"
+        mkdir -p "$out/share/applications"
+        ${with final; lib.getExe envsubst} < "crates/zed/resources/zed.desktop.in" > "$out/share/applications/zed.desktop"
+      )
+    '';
   };
 }
