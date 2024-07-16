ef5649822f2acd8abfb15403f4d746f4b952ef19
RazTheCat <razthecat@posteo.net>
2024-07-15 20:52:56 -0400

    scx 1.0.1 version bump.

--- pkgs/scx/common.nix
+++ pkgs/scx/common.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
 rec {
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "sched-ext";
     repo = "scx";
     rev = "refs/tags/v${version}";
-    hash = "sha256-VSpuEYgOIE8sxcUL+D8os5TF6JVYFzWXj23Qe5gxc3Y=";
+    hash = "sha256-4uM69RJdRm75z595QIA/zJYl6b00HqT363vfoXIRqzY=";
     fetchSubmodules = true;
   };
 }
