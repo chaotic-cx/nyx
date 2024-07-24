fef73d4339b36cbec290c43f4999711e06f4f56e
PedroHLC <root@pedrohlc.com>
2024-07-24 10:49:11 -0300

    nix-flake-schemas_git: 20240624142828-8d2c6be -> 20240712141454-e1ed2d1

--- pkgs/nix-flake-schemas-git/default.nix
+++ pkgs/nix-flake-schemas-git/default.nix
@@ -1,8 +1,8 @@
-{ prev, gitOverride, ... }:
+{ final, prev, gitOverride, ... }:
 
 gitOverride {
   nyxKey = "nix-flake-schemas_git";
-  prev = prev.nixVersions.nix_2_20;
+  prev = prev.nixVersions.nix_2_22;
 
   versionNyxPath = "pkgs/nix-flake-schemas-git/version.json";
   fetcher = "fetchFromGitHub";
@@ -14,6 +14,14 @@ gitOverride {
 
   postOverride = prevAttrs: {
     doInstallCheck = false;
+    configureFlags = prevAttrs.configureFlags ++ [
+      "--with-default-flake-schemas=${final.fetchFromGitHub {
+        owner = "DeterminateSystems";
+        repo = "flake-schemas";
+        rev = "v0.1.3";
+        hash = "sha256-c2AZH9cOnSpPXV8Lwy19/I8EgW7G+E+Zh6YQBZZwzxI=";
+      }}"
+    ];
     meta = prevAttrs.meta // {
       homepage = "https://determinate.systems/posts/flake-schemas";
       description = "Nix from the branch with flake-schemas";
