cfc4c3664f74df6a931011ac5801c7504579009a
TNE <tne@garudalinux.org>
2024-08-11 00:29:34 +0200

    discord-krisp: Fix patch for newer discord versions

--- pkgs/discord-krisp/default.nix
+++ pkgs/discord-krisp/default.nix
@@ -1,19 +1,22 @@
 { prev, ... }:
 let
-  patch-krisp = prev.writeScript "patch-krisp" ''
-    discord_version="${prev.discord.version}"
-    file="$HOME/.config/discord/$discord_version/modules/discord_krisp/discord_krisp.node"
-    if [ -f "$file" ]; then
-    addr=$("${prev.rizin}/bin/rz-find" -x '4881ec00010000' "$file" | head -n1)
-    "${prev.rizin}/bin/rizin" -q -w -c "s $addr + 0x30 ; wao nop" "$file"
-    fi
-  '';
+  # krisp-patcher.py courtesy of https://github.com/sersorrel/sys
+  patch-krisp = prev.writers.writePython3 "krisp-patcher" {
+    libraries = with prev.python3Packages; [ capstone pyelftools ];
+    # Ignore syntax checker error codes that affect krisp-patcher.py
+    flakeIgnore = [
+      "E501"
+      "F403"
+      "F405"
+    ];
+  } (builtins.readFile ./krisp-patcher.py);
   binaryName = "Discord";
+  node_module="\\$HOME/.config/discord/${prev.discord.version}/modules/discord_krisp/discord_krisp.node";
 in
 prev.discord.overrideAttrs (previousAttrs: {
   postInstall = previousAttrs.postInstall + ''
     wrapProgramShell $out/opt/${binaryName}/${binaryName} \
-    --run "${patch-krisp}"
+    --run "${patch-krisp} ${node_module}"
   '';
   passthru = removeAttrs previousAttrs.passthru [ "updateScript" ];
   meta = {
