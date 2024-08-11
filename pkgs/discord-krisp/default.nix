c69597dc8a8391257f8f881b10a7628f13125123
PedroHLC <root@pedrohlc.com>
2024-08-11 12:12:08 -0300

    fmt

--- pkgs/discord-krisp/default.nix
+++ pkgs/discord-krisp/default.nix
@@ -1,17 +1,19 @@
 { prev, ... }:
 let
   # krisp-patcher.py courtesy of https://github.com/sersorrel/sys
-  patch-krisp = prev.writers.writePython3 "krisp-patcher" {
-    libraries = with prev.python3Packages; [ capstone pyelftools ];
-    # Ignore syntax checker error codes that affect krisp-patcher.py
-    flakeIgnore = [
-      "E501"
-      "F403"
-      "F405"
-    ];
-  } (builtins.readFile ./krisp-patcher.py);
+  patch-krisp = prev.writers.writePython3 "krisp-patcher"
+    {
+      libraries = with prev.python3Packages; [ capstone pyelftools ];
+      # Ignore syntax checker error codes that affect krisp-patcher.py
+      flakeIgnore = [
+        "E501"
+        "F403"
+        "F405"
+      ];
+    }
+    (builtins.readFile ./krisp-patcher.py);
   binaryName = "Discord";
-  node_module="\\$HOME/.config/discord/${prev.discord.version}/modules/discord_krisp/discord_krisp.node";
+  node_module = "\\$HOME/.config/discord/${prev.discord.version}/modules/discord_krisp/discord_krisp.node";
 in
 prev.discord.overrideAttrs (previousAttrs: {
   postInstall = previousAttrs.postInstall + ''
