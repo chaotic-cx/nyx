a34de601c578e144d47ee3bbb15e8fd3c5221750
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-09-09 19:02:16 +0300

    scx: 1.0.3. -> 1.0.4

--- pkgs/scx/common.nix
+++ pkgs/scx/common.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
 rec {
-  version = "1.0.3";
+  version = "1.0.4";
 
   src = fetchFromGitHub {
     owner = "sched-ext";
     repo = "scx";
     rev = "refs/tags/v${version}";
-    hash = "sha256-r4LhgnFJSWnDAx8x6jY4wgTUSoGfN80vYIrZqKWg4m8=";
+    hash = "sha256-FSeg4E9tpV9KZwVvwPKpcR6TD1wh8g+WjkNYnZeJnuY=";
     fetchSubmodules = true;
   };
 }
