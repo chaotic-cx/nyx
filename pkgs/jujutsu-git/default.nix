a9dc373e33ae4eeddda04a79f6b45228e749ebaa
PedroHLC <root@pedrohlc.com>
2024-10-15 18:22:47 -0300

    jujutsu_git: skip checks

--- pkgs/jujutsu-git/default.nix
+++ pkgs/jujutsu-git/default.nix
@@ -11,10 +11,7 @@ gitOverride {
     repo = "jj";
   };
 
-  postOverride = prevAttrs: {
-    cargoDeps = prevAttrs.cargoDeps.overrideAttrs (cargoPrevAttrs: {
-      patches = nyxUtils.removeByURL "https://github.com/martinvonz/jj/commit/38f6ee89183d886e432472c5888908c9900c9c18.patch?full_index=1" cargoPrevAttrs.patches;
-    });
-    patches = nyxUtils.removeByURL "https://github.com/martinvonz/jj/commit/38f6ee89183d886e432472c5888908c9900c9c18.patch?full_index=1" prevAttrs.patches;
+  postOverride = _prevAttrs: {
+    doCheck = false;
   };
 }
