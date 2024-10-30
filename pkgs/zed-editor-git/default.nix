bb38ae40cca5f8cff2a59e643e20f1957a32a460
PedroHLC <root@pedrohlc.com>
2024-10-30 16:47:20 -0300

    zed-editor_git: workaround zed#19971 and rebase

--- pkgs/zed-editor-git/default.nix
+++ pkgs/zed-editor-git/default.nix
@@ -23,26 +23,10 @@ gitOverride {
   };
 
   postOverride = prevAttrs: {
-    env = prevAttrs.env // { OPENSSL_NO_VENDOR = 1; };
     # Nothing wrong on it, just saving compilation time for me!
     dontCheck = true;
     doInstallCheck = false;
-    # https://github.com/zed-industries/zed/issues/15902
-    RUSTFLAGS = "-Clink-arg=-z -Clink-arg=nostart-stop-gc " + prevAttrs.RUSTFLAGS;
-    # Starting zed-editor from zed seems to loose these libraries somehow
-    nativeBuildInputs = with final; [ makeWrapper cmake ] ++ prevAttrs.nativeBuildInputs;
-    postInstall = with final; ''
-      wrapProgram $out/bin/zeditor \
-        --prefix LD_PRELOAD : ${alsa-lib}/lib/libasound.so.2 \
-        --prefix LD_PRELOAD : ${zstd.out}/lib/libzstd.so.1 \
-        --prefix LD_PRELOAD : ${openssl.out}/lib/libssl.so.3 \
-        --prefix LD_PRELOAD : ${openssl.out}/lib/libcrypto.so.3 \
-        --prefix LD_PRELOAD : ${zlib}/lib/libz.so.1 \
-        --prefix LD_PRELOAD : ${xorg.libxcb}/lib/libxcb.so.1 \
-        --prefix LD_PRELOAD : ${libxkbcommon}/lib/libxkbcommon.so.0 \
-        --prefix LD_PRELOAD : ${libxkbcommon}/lib/libxkbcommon-x11.so.0 \
-        --prefix LD_PRELOAD : ${curl.out}/lib/libcurl.so.4 \
-        --prefix LD_PRELOAD : ${libgit2.lib}/lib/libgit2.so.1.8
-    '';
+    # https://github.com/zed-industries/zed/issues/19971
+    preBuild = "";
   };
 }
