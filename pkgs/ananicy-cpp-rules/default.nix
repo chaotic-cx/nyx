d32e7c1946d968a7a670c316a22fdc5203b33638
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-11-01 01:22:39 +0530

    ananicy_cpp-rules -> ananicy-rules-cachyos_git: move to gitOverride
    It's packaged in nixpkgs, so we could just use an override.
    This is a breaking change.

--- pkgs/ananicy-cpp-rules/default.nix
+++ pkgs/ananicy-cpp-rules/default.nix
@@ -1,43 +1,14 @@
-{ lib
-, stdenvNoCC
-, callPackage
-, fetchFromGitHub
-, ...
-}:
-let
-  current = lib.trivial.importJSON ./version.json;
-in
-stdenvNoCC.mkDerivation rec {
-  pname = "ananicy-cpp-rules";
-  inherit (current) version;
+{ prev, gitOverride, ... }:
 
-  src = fetchFromGitHub {
-    inherit (current) rev hash;
+gitOverride {
+  nyxKey = "ananicy-rules-cachyos";
+  prev = prev.ananicy-rules-cachyos;
+
+  versionNyxPath = "pkgs/ananicy-cpp-rules/version.json";
+  fetcher = "fetchFromGitHub";
+  fetcherData = {
     owner = "CachyOS";
     repo = "ananicy-rules";
   };
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/etc/ananicy.d
-    rm README.md LICENSE
-    cp -r * $out/etc/ananicy.d
-    runHook postInstall
-  '';
-
-  passthru.updateScript = callPackage ../../shared/git-update.nix {
-    inherit pname;
-    nyxKey = "ananicy-cpp-rules";
-    versionPath = "pkgs/ananicy-cpp-rules/version.json";
-    fetchLatestRev = callPackage ../../shared/github-rev-fetcher.nix { } "master" src;
-    gitUrl = src.gitRepoUrl;
-  };
-
-  meta = with lib; {
-    description = "CachyOS' ananicy-rules meant to be used with ananicy-cpp";
-    homepage = "https://github.com/CachyOS/ananicy-rules";
-    license = licenses.gpl3;
-    maintainers = [ maintainers.dr460nf1r3 ];
-    platforms = platforms.all;
-  };
+  ref = "master";
 }
