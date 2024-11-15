d9d59a2819215227199b5eeab06a7e5fb56681f7
PedroHLC <root@pedrohlc.com>
2024-11-15 10:46:43 -0300

    niri_git: rebase

--- pkgs/niri-git/default.nix
+++ pkgs/niri-git/default.nix
@@ -1,4 +1,4 @@
-{ final, prev, gitOverride, niriPins, ... }:
+{ final, prev, gitOverride, nyxUtils, niriPins, ... }:
 
 gitOverride {
   nyxKey = "niri_git";
@@ -18,5 +18,8 @@ gitOverride {
 
   postOverride = prevAttrs: {
     buildInputs = [ final.libdisplay-info ] ++ prevAttrs.buildInputs;
+    patches = nyxUtils.removeByURL
+      "https://github.com/YaLTeR/niri/commit/1951d2a9f262196a706f2645efb18dac3c4d6839.patch"
+      prevAttrs.patches;
   };
 }
