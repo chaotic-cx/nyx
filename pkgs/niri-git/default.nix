054b38a112e67245a20032662ea0231641458469
PedroHLC <root@pedrohlc.com>
2025-02-24 11:33:30 -0300

    niri_git: disable version check

--- pkgs/niri-git/default.nix
+++ pkgs/niri-git/default.nix
@@ -16,5 +16,6 @@ gitOverride {
     patches = nyxUtils.removeByURL
       "https://github.com/YaLTeR/niri/commit/1951d2a9f262196a706f2645efb18dac3c4d6839.patch"
       prevAttrs.patches;
+    nativeInstallCheckInputs = [ ];
   };
 }
