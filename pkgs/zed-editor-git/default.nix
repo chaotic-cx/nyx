af9b9bd6669b623f724aaaf59d0c2f90f58025db
TNE <tne@garudalinux.org>
2025-03-01 12:57:36 +0100

    zed-editor_git: bump

--- pkgs/zed-editor-git/default.nix
+++ pkgs/zed-editor-git/default.nix
@@ -23,26 +23,20 @@ gitOverride (current: {
     };
     patches =
       let
+        revertZed25516 =
+          final.fetchpatch2 {
+            url = "https://github.com/zed-industries/zed/commit/17a483cb0319d3454e9528ad1de3e3c5a7cd4d51.patch";
+            hash = "sha256-isf/QNzpRCxKdTrZDIQMWcZFnmv/0LsG7se53cj1Plo=";
+            revert = true;
+          };
         revertZed25209 =
           final.fetchpatch2 {
             url = "https://github.com/zed-industries/zed/commit/1087e05da49bc788f1df81e0aa26ac66c3c73789.patch";
             hash = "sha256-pihAhFQHKNr9LCw1REWAG9WR/WK5z47vh07+iHT264k=";
             revert = true;
           };
-        revertZed25035 =
-          final.fetchpatch2 {
-            url = "https://github.com/zed-industries/zed/commit/5eadeb67b0446caf4fbac03282ed031157474eae.patch";
-            hash = "sha256-ccu8DLpehC3f4Yw4q9MHAXF5inkzNuap8jW/E4CAZ0Q=";
-            revert = true;
-          };
-        revertZed24996 =
-          final.fetchpatch2 {
-            url = "https://github.com/zed-industries/zed/commit/bcba0b92ed7842310959f82cc9ee93673c5dba87.patch";
-            hash = "sha256-y4EZhZ2KPvOoginx5EfgGBas/PGOLYkhbS0Cvp8rh5g=";
-            revert = true;
-          };
       in
-      [ revertZed25209 revertZed25035 revertZed24996 ] ++ prevAttrs.patches;
+      [ revertZed25516 revertZed25209 ] ++ prevAttrs.patches;
     nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ final.clang ];
     # Nothing wrong on it, just saving compilation time for me!
     dontCheck = true;
