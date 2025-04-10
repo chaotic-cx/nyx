72d8607e498dc1fe463ae5bfec8b590a138fd1db
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2025-04-10 21:32:38 +0530

    openrgb_git: init (#1019)

---
+++ pkgs/openrgb-git/default.nix
@@ -0,0 +1,105 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  qt6Packages,
+  libusb1,
+  hidapi,
+  pkg-config,
+  coreutils,
+  mbedtls_2,
+  symlinkJoin,
+  callPackage,
+}:
+
+let
+  current = lib.importJSON ./version.json;
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "openrgb";
+  inherit (current) version;
+
+  src = fetchFromGitLab {
+    owner = "CalcProgrammer1";
+    repo = "OpenRGB";
+    inherit (current) rev hash;
+  };
+
+  nativeBuildInputs =
+    [
+      pkg-config
+    ]
+    ++ (with qt6Packages; [
+      qmake
+      wrapQtAppsHook
+    ]);
+
+  buildInputs =
+    [
+      libusb1
+      hidapi
+      mbedtls_2
+    ]
+    ++ (with qt6Packages; [
+      qtbase
+      qttools
+    ]);
+
+  postPatch = ''
+    patchShebangs scripts/build-udev-rules.sh
+    substituteInPlace scripts/build-udev-rules.sh \
+      --replace-fail '/usr/bin/env chmod' "${coreutils}/bin/chmod"
+    substituteInPlace OpenRGB.pro \
+    --replace-fail "lrelease" "${qt6Packages.qttools.dev}/bin/lrelease"
+  '';
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    HOME=$TMPDIR $out/bin/openrgb --help > /dev/null
+  '';
+
+  passthru.withPlugins =
+    plugins:
+    let
+      pluginsDir = symlinkJoin {
+        name = "openrgb-plugins";
+        paths = plugins;
+        # Remove all library version symlinks except one,
+        # or they will result in duplicates in the UI.
+        # We leave the one pointing to the actual library, usually the most
+        # qualified one (eg. libOpenRGBHardwareSyncPlugin.so.1.0.0).
+        postBuild = ''
+          for f in $out/lib/*; do
+            if [ "$(dirname $(readlink "$f"))" == "." ]; then
+              rm "$f"
+            fi
+          done
+        '';
+      };
+    in
+    finalAttrs.finalPackage.overrideAttrs (old: {
+      qmakeFlags = old.qmakeFlags or [ ] ++ [
+        # Welcome to Escape Hell, we have backslashes
+        ''DEFINES+=OPENRGB_EXTRA_PLUGIN_DIRECTORY=\\\""${
+          lib.escape [ "\\" "\"" " " ] (toString pluginsDir)
+        }/lib\\\""''
+      ];
+    });
+
+  passthru.updateScript = callPackage ../../shared/git-update.nix {
+    inherit (finalAttrs) pname;
+    nyxKey = "openrgb_git";
+    versionPath = "pkgs/openrgb-git/version.json";
+    fetchLatestRev = callPackage ../../shared/github-rev-fetcher.nix { } "master" finalAttrs.src;
+    gitUrl = finalAttrs.src.gitRepoUrl;
+  };
+
+  meta = {
+    description = "Open source RGB lighting control";
+    homepage = "https://gitlab.com/CalcProgrammer1/OpenRGB";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ johnrtitor ];
+    mainProgram = "openrgb";
+  };
+})
