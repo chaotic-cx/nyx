e696dbda293492a4951872a7dced61695472eaef
PedroHLC <root@pedrohlc.com>
2025-04-29 19:07:04 -0300

    helix_git: move away from fetchTree

--- pkgs/helix-git/grammar-artifact.nix
+++ pkgs/helix-git/grammar-artifact.nix
@@ -1,9 +1,44 @@
-{ makeGrammar, source }:
+{
+  makeGrammar,
+  source,
+  fetchgit,
+  fetchFromGitHub,
+}:
 
+let
+  nixpkgsFetcher =
+    {
+      narHash,
+      type,
+      owner ? null,
+      repo ? null,
+      rev ? null,
+      shallow ? true,
+      url ? null,
+      ref ? "HEAD",
+    }@input:
+    if !shallow then
+      throw "Not shallow is unsupported"
+    else if ref != "HEAD" then
+      throw "Unsupported ref"
+    else if type == "github" then
+      fetchFromGitHub {
+        inherit owner repo rev;
+        hash = narHash;
+      }
+    else if type == "git" then
+      fetchgit {
+        inherit url rev;
+        name = "source";
+        hash = narHash;
+      }
+    else
+      throw "Unsupported type";
+in
 (makeGrammar {
   language = source.name;
   version = source.rev;
-  src = builtins.fetchTree (
+  src = nixpkgsFetcher (
     builtins.removeAttrs source [
       "name"
       "lastModified"
