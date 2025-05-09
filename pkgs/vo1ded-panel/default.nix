d1e31c715b94941597cf71d923a2fdf9fbea9c46
Filip Myslinski <filipmyslinski2006@gmail.com>
2025-05-10 00:13:05 +0200

    vo1ded-panel: added vo1ded-panel package

---
+++ pkgs/vo1ded-panel/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  agsBundle
+}:
+let
+  current = pkgs.lib.trivial.importJSON ./version.json;
+  src = pkgs.fetchFromGitHub {
+    inherit (current) rev hash;
+    owner = "FilipTLW";
+    repo = "vo1ded-panel";
+  };
+in
+agsBundle {
+  inherit src;
+  name = "vo1ded-panel";
+  extraPackages = with pkgs; [
+    astal_tray
+    astal_hyprland
+  ];
+}
