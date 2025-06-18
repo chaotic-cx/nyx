fb70a6aacaa230aa48e0fc18cab2c5ece179bac5
PedroHLC <root@pedrohlc.com>
2025-06-18 17:25:43 -0300

    pcsx2_git: rebase nixpkgs

--- pkgs/pcsx2-git/default.nix
+++ pkgs/pcsx2-git/default.nix
@@ -9,10 +9,6 @@ gitOverride {
   nyxKey = "pcsx2_git";
   prev = prev.pcsx2;
 
-  newInputs = with final; {
-    SDL2 = sdl3;
-  };
-
   versionNyxPath = "pkgs/pcsx2-git/version.json";
   fetcher = "fetchFromGitHub";
   fetcherData = {
@@ -20,27 +16,4 @@ gitOverride {
     repo = "pcsx2";
   };
   ref = "master";
-
-  postOverride = prevAttrs: {
-    buildInputs =
-      prevAttrs.buildInputs
-      ++ (
-        with final;
-        let
-          plutosvg_ = plutosvg.overrideAttrs (prevAttrs: {
-            postInstall =
-              (prevAttrs.postInstall or "")
-              + ''
-                substituteInPlace $out/lib/cmake/plutosvg/plutosvgTargets.cmake \
-                  --replace-fail "\''${_IMPORT_PREFIX}/include/" "$dev/include/"
-              '';
-          });
-        in
-        [
-          kddockwidgets
-          plutovg
-          plutosvg_
-        ]
-      );
-  };
 }
