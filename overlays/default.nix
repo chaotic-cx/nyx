41a000489d5a84e18d22b9dca682cf479a0541cf
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-06-29 17:46:50 -0400

    Add bazaar (flatpak gui store) (#1095)

--- overlays/default.nix
+++ overlays/default.nix
@@ -139,6 +139,8 @@ in
 
   appmenu-gtk3-module = final.callPackage ../pkgs/appmenu-gtk3-module { };
 
+  bazaar_git = final.callPackage ../pkgs/bazaar-git { };
+
   beautyline-icons = final.callPackage ../pkgs/beautyline-icons { };
 
   bpftools_full =
---
+++ pkgs/bazaar-git/default.nix
@@ -0,0 +1,84 @@
+{
+  lib,
+  callPackage,
+  blueprint-compiler,
+  desktop-file-utils,
+  fetchFromGitHub,
+  flatpak,
+  flatpak-xdg-utils,
+  glib,
+  libxmlb,
+  libglycin,
+  cmake,
+  gobject-introspection,
+  gtk4,
+  libdex,
+  libadwaita,
+  appstream,
+  meson,
+  ninja,
+  pkg-config,
+  libyaml,
+  libsoup_3,
+  stdenv,
+  wrapGAppsHook4,
+  json-glib,
+}:
+
+let
+  current = lib.trivial.importJSON ./version.json;
+in
+stdenv.mkDerivation rec {
+  pname = "bazaar";
+  inherit (current) version;
+
+  src = fetchFromGitHub {
+    inherit (current) rev hash;
+    owner = "kolunmi";
+    repo = "bazaar";
+  };
+
+  nativeBuildInputs = [
+    blueprint-compiler
+    desktop-file-utils
+    meson
+    cmake
+    ninja
+    pkg-config
+    wrapGAppsHook4
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    flatpak
+    flatpak-xdg-utils
+    glib
+    gtk4
+    libadwaita
+    libdex
+    appstream
+    libxmlb
+    libglycin
+    libyaml
+    json-glib
+    libsoup_3
+  ];
+
+  passthru.updateScript = callPackage ../../shared/git-update.nix {
+    inherit pname;
+    nyxKey = "bazaar_git";
+    versionPath = "pkgs/bazaar-git/version.json";
+    fetchLatestRev = callPackage ../../shared/github-rev-fetcher.nix { } "master" src;
+    gitUrl = src.gitRepoUrl;
+  };
+
+  meta = {
+    changelog = "https://github.com/kolunmi/bazaar/commits/master/";
+    description = "A new app store idea for GNOME";
+    homepage = "https://github.com/kolunmi/bazaar";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "bazaar";
+    maintainers = with lib.maintainers; [ jumpyvi ];
+    platforms = lib.platforms.linux;
+  };
+}
