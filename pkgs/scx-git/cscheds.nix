06bf97ab4d02f3cf17af3936ce17ea7e94dc8226
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-08-16 00:58:20 +0530

    scx_git: fix build

--- pkgs/scx-git/cscheds.nix
+++ pkgs/scx-git/cscheds.nix
@@ -1,9 +1,11 @@
 {
   scx,
   scx-common,
+  nyxUtils,
+  lib
 }:
 
-scx.cscheds.overrideAttrs (_prevAttrs: {
+scx.cscheds.overrideAttrs (prevAttrs: {
   inherit (scx-common)
     version
     src
@@ -12,11 +14,13 @@ scx.cscheds.overrideAttrs (_prevAttrs: {
     libbpf_src
     ;
   #
-  postPatch = builtins.replaceStrings [ "--replace-fail" ] [ "--replace-warn" ] _prevAttrs.postPatch;
+  postPatch = builtins.replaceStrings [ "--replace-fail" ] [ "--replace-warn" ] prevAttrs.postPatch;
   # Cherry-picks nixpkgs#424862
   preInstall = "";
   postFixup = ''
     mkdir -p ${placeholder "dev"}
     cp -r libbpf ${placeholder "dev"}
   '';
+
+  mesonFlags = lib.lists.remove "-Dlibalpm=disabled" (lib.flatten prevAttrs.mesonFlags);
 })
