ca57e7f9aa014e4afc5e3e8d57411d535eb0a924
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-08-16 01:03:40 +0530

    scx_git: remove previous workaround
    PR merged upstream

--- pkgs/scx-git/cscheds.nix
+++ pkgs/scx-git/cscheds.nix
@@ -1,8 +1,7 @@
 {
   scx,
   scx-common,
-  nyxUtils,
-  lib
+  lib,
 }:
 
 scx.cscheds.overrideAttrs (prevAttrs: {
@@ -13,14 +12,8 @@ scx.cscheds.overrideAttrs (prevAttrs: {
     bpftools_src
     libbpf_src
     ;
-  #
+
   postPatch = builtins.replaceStrings [ "--replace-fail" ] [ "--replace-warn" ] prevAttrs.postPatch;
-  # Cherry-picks nixpkgs#424862
-  preInstall = "";
-  postFixup = ''
-    mkdir -p ${placeholder "dev"}
-    cp -r libbpf ${placeholder "dev"}
-  '';
 
   mesonFlags = lib.lists.remove "-Dlibalpm=disabled" (lib.flatten prevAttrs.mesonFlags);
 })
