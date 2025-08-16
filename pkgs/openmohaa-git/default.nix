9704092b7d25b14487aa04232f4c88169e764219
PedroHLC <root@pedrohlc.com>
2025-08-16 07:07:13 -0300

    openmohaa_git: add new dependencies

--- pkgs/openmohaa-git/default.nix
+++ pkgs/openmohaa-git/default.nix
@@ -14,4 +14,14 @@ gitOverride {
     owner = "openmoh";
     repo = "openmohaa";
   };
+
+  postOverride = prevAttrs: {
+    buildInputs =
+      prevAttrs.buildInputs
+      ++ (with final; [
+        freetype
+        libmad
+      ]);
+    cmakeFlags = prevAttrs.cmakeFlags ++ [ "-DUSE_INTERNAL_JPEG=1" ];
+  };
 }
