11e2c38094f84f4ed8193fe68fb0bb37f3158de1
PedroHLC <root@pedrohlc.com>
2025-08-16 07:53:45 -0300

    qtile_git: adopt upstream changes

--- pkgs/qtile-git/default.nix
+++ pkgs/qtile-git/default.nix
@@ -23,6 +23,21 @@ gitOverride {
   postOverride = prevAttrs: {
     name = prevAttrs.name + ".99";
     patches = [ ];
+    postPatch = "";
+    pypaBuildFlags =
+      with final;
+      map (x: "--config-setting=${x}") [
+        "PANGO_PATH=${pango.out}/lib/libpango-1.0.so.0"
+        "PANGOCAIRO_PATH=${pango.out}/lib/libpangocairo-1.0.so.0"
+        "GOBJECT_PATH=${glib.out}/lib/libgobject-2.0.so.0"
+        "XCBCURSOR_PATH=${xorg.xcbutilcursor.out}/lib/libxcb-cursor.so.0"
+      ];
+    env =
+      (prevAttrs.env or { })
+      // (with final; {
+        QTILE_PIXMAN_PATH = "${lib.getDev pixman}/include";
+        QTILE_LIBDRM_PATH = "${lib.getDev libdrm}/include/libdrm";
+      });
     passthru = prevAttrs.passthru // {
       tests.smoke-test = import ./test.nix {
         inherit (flakes) nixpkgs;
