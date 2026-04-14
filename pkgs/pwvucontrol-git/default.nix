fa2bfd7af1fbce9d9be109ab34fab95a1d4aef58
loner <2788892716@qq.com>
2026-04-15 00:19:59 +0800

    fix(pwvucontrol): fix build failure due to missing blueprint-compiler

--- pkgs/pwvucontrol-git/default.nix
+++ pkgs/pwvucontrol-git/default.nix
@@ -27,4 +27,14 @@ gitOverride {
     owner = "saivert";
     repo = "pwvucontrol";
   };
+
+  postOverride = prevAttrs: {
+    # Add blueprint-compiler to build inputs so Meson finds it at configuration time
+    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ final.blueprint-compiler ];
+
+    mesonFlags = (prevAttrs.mesonFlags or [ ]) ++ [
+      # We set wrap_mode to 'nodownload' to prevent Meson from trying to download
+      "-Dwrap_mode=nodownload"
+    ];
+  };
 }
