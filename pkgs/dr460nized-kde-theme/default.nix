9d668280de18f6592b86e92272c4c538d3122bc6
loner <2788892716@qq.com>
2026-04-26 08:44:42 +0800

    dr460nized-kde-theme: migrate to fetchgit, fix install

--- pkgs/dr460nized-kde-theme/default.nix
+++ pkgs/dr460nized-kde-theme/default.nix
@@ -1,28 +1,36 @@
 {
-  beautyline-icons,
-  callPackage,
-  fetchFromGitLab,
-  fetchurl,
   lib,
-  plasma-plugin-blurredwallpaper,
   stdenvNoCC,
+
+  fetchgit,
+  fetchurl,
+  callPackage,
+
+  beautyline-icons,
+  plasma-plugin-blurredwallpaper,
   sweet-nova,
 }:
 
 let
   current = lib.trivial.importJSON ./version.json;
+
   srcMeta = {
-    inherit (current) rev hash;
     group = "garuda-linux";
     owner = "themes-and-settings/settings";
     repo = "garuda-dr460nized";
   };
+
+  gitUrl = "https://gitlab.com/${srcMeta.group}/${srcMeta.owner}/${srcMeta.repo}.git";
 in
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dr460nized-kde-theme";
   inherit (current) version;
 
-  src = fetchFromGitLab srcMeta;
+  src = fetchgit {
+    url = gitUrl;
+    rev = current.rev;
+    sha256 = current.hash;
+  };
 
   malefor = fetchurl {
     url = "https://gitlab.com/garuda-linux/themes-and-settings/artwork/garuda-wallpapers/-/raw/master/src/garuda-wallpapers/Malefor.jpg";
@@ -35,43 +43,57 @@ stdenvNoCC.mkDerivation rec {
     sweet-nova
   ];
 
-  installPhase = ''
-    runHook preInstall
-    install -d $out/skel
-    cp -r etc/skel $out/
-    install -d $out/share
-    cp -r usr/share/* $out/share/
-    install -Dm644 $malefor $out/share/wallpapers/garuda-wallpapers/Malefor.jpg
-    runHook postInstall
-  '';
   postPatch = ''
     for file in $(find ./* \( -type f \( -name "*.profile" -o -name "*.conf" -o ! -name "*.*" \) \) -o -type l ); do
-      if [ -h $file ]; then
-        ln -fs $(readlink $file | sed -e 's|/usr/share|/run/current-system/sw/share|g') $file
+      if [ -h "$file" ]; then
+        ln -fs "$(readlink "$file" | sed -e 's|/usr/share|/run/current-system/sw/share|g')" "$file"
       else
-        substituteInPlace $file --replace "/usr/bin" "/run/current-system/sw/bin" --replace "/usr/share" "/run/current-system/sw/share"
+        substituteInPlace "$file" \
+          --replace "/usr/bin" "/run/current-system/sw/bin" \
+          --replace "/usr/share" "/run/current-system/sw/share"
       fi
     done
 
-    substituteInPlace usr/share/plasma/{look-and-feel/Dr460nized/contents/layouts/org.kde.plasma.desktop-layout.js,layout-templates/org.garuda.desktop.defaultDock/contents/layout.js} \
+    substituteInPlace \
+      usr/share/plasma/look-and-feel/Dr460nized/contents/layouts/org.kde.plasma.desktop-layout.js \
+      usr/share/plasma/layout-templates/org.garuda.desktop.defaultDock/contents/layout.js \
       --replace "applications:garuda-welcome.desktop," "" \
       --replace "applications:snapper-tools.desktop," "" \
       --replace ",applications:octopi.desktop" ""
   '';
 
+  installPhase = ''
+    runHook preInstall
+
+    install -d $out/skel
+    if [ -d etc/skel ]; then
+      cp -r etc/skel/. $out/skel/
+    fi
+
+    install -d $out/share
+    if [ -d usr/share ]; then
+      cp -r usr/share/. $out/share/
+    fi
+
+    install -Dm644 $malefor \
+      $out/share/wallpapers/garuda-wallpapers/Malefor.jpg
+
+    runHook postInstall
+  '';
+
   passthru.updateScript = callPackage ../../shared/git-update.nix {
-    inherit pname;
+    pname = finalAttrs.pname;
     nyxKey = "dr460nized-kde-theme";
     versionPath = "pkgs/dr460nized-kde-theme/version.json";
     fetchLatestRev = callPackage ../../shared/gitlab-rev-fetcher.nix { } "main" srcMeta;
-    gitUrl = src.gitRepoUrl;
+    inherit gitUrl;
   };
 
-  meta = with lib; {
+  meta = {
     description = "The default Garuda dr460nized theme";
     homepage = "https://gitlab.com/garuda-linux/themes-and-settings/settings/garuda-dr460nized";
-    license = licenses.gpl3Only;
-    maintainers = [ maintainers.dr460nf1r3 ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Only;
+    maintainers = [ lib.maintainers.dr460nf1r3 ];
+    platforms = lib.platforms.linux;
   };
-}
+})
