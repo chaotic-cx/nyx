21a8ef816f34558a438d778057a8809322ea2415
PedroHLC <root@pedrohlc.com>
2026-07-17 17:03:24 -0300

    failures: update aarch64-{darwin,linux}

--- maintenance/failures.aarch64-darwin.nix
+++ maintenance/failures.aarch64-darwin.nix
@@ -1,16 +1,11 @@
 {
   "appmenu-gtk3-module" = "/nix/store/zc2kk54s6xms1j7i8834wnaay1pscn53-appmenu-gtk3-module-0.7.6";
   "discord-krisp" = "/nix/store/xg6pmxss890xcwaqg2g7qarn3d1mnz7x-discord-0.0.398";
-  "firedragon-bin" = "/nix/store/hfg0zrnwhv7kvwp5d96zj0mismw6cwja-firedragon-bin-12.9.1";
-  "firedragon-catppuccin-bin" = "/nix/store/ac3xh9smghbvxcczaz6dkpi338bdk29y-firedragon-catppuccin-bin-12.9.1";
-  "helix_git" = "/nix/store/s5qmixnfpxy5ps6i396fb83iyisz95ij-helix-unwrapped-unstable-20260706194641-14d6bc0";
-  "lan-mouse_git" = "/nix/store/yib4r19l1ip0v3i4hz4jxn2c0g62q3rm-lan-mouse-unstable-20260628142239-392af44";
-  "mesa_git" = "/nix/store/c9jzfr6lz362g7k3w31vbgvbssn1brxx-mesa-f768c9";
-  "mpv-vapoursynth" = "/nix/store/s79siyl1ixjsq151rv54cpcry4p3xaca-mpv-with-scripts-0.41.0";
-  "spirv-headers_git" = "/nix/store/2s8sx8rd97rxpfadl9mqzydw2s354v5v-spirv-headers-unstable-20260708154953-29981f6";
-  "telegram-desktop-unwrapped_git" = "/nix/store/g1ssrgi9849n3f85g2v7z8cy3rg018yw-telegram-desktop-unwrapped-unstable-20260709183339-e01ee72";
-  "telegram-desktop_git" = "/nix/store/v06mfj1kbcpmqykjyjjdicl0ffj3mhh0-telegram-desktop-unstable-20260709183339-e01ee72";
+  "firedragon-bin" = "/nix/store/hm3gbmfhc6n1ck4z453g8ahy1bxy8pxg-firedragon-bin-12.9.1";
+  "firedragon-catppuccin-bin" = "/nix/store/c08gmd990xpgylwi7niyaaky09l1qg7x-firedragon-catppuccin-bin-12.9.1";
+  "helix_git" = "/nix/store/pidddyfxirmrf4kpqzaf2z0qy1si3qf9-helix-unwrapped-unstable-20260716163018-f6f3eb1";
+  "lan-mouse_git" = "/nix/store/2rmcwyc0wyb46ns7cclpi4f5wynf9wxn-lan-mouse-unstable-20260628142239-392af44";
+  "mesa_git" = "/nix/store/fyfy7f4pnfig8p8lmibr5mqgk4yvj9g4-mesa-99bf1b";
   "vulkanPackages_latest.vulkan-tools" = "/nix/store/4gdn2ciykjv9pwfidynn30bydj3cw63h-vulkan-tools-1.4.356";
-  "yt-dlp_git" = "/nix/store/j457zyg7k900pdv76ii7c5hc1hmz8x3m-yt-dlp-unstable-20260709231100-59d9ae6";
   "zed-editor-fhs_git" = "skip";
 }
--- maintenance/failures.aarch64-linux.nix
+++ maintenance/failures.aarch64-linux.nix
@@ -1,6 +1,4 @@
 {
-  "spirv-headers_git" = "/nix/store/x5hhxyjgxczfwn68fpsj2i1wx3wmxz5r-spirv-headers-unstable-20260708154953-29981f6";
   "telegram-desktop-unwrapped_git" = "skip";
-  "telegram-desktop_git" = "/nix/store/w4i2nrz6pxwdmlqb1dk2dk3gq8cl5ljr-telegram-desktop-unstable-20260709183339-e01ee72";
-  "yt-dlp_git" = "/nix/store/92r3v0md4dc5xyr7dss7xznb7415v6cz-yt-dlp-unstable-20260709231100-59d9ae6";
+  "telegram-desktop_git" = "skip";
 }
