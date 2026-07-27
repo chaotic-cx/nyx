2cff39bbc6a40c7c842de01a8b2c8bd94aa3a1cd
PedroHLC <root@pedrohlc.com>
2026-07-27 16:03:52 -0300

    failures: update aarch64-{darwin,linux}

--- maintenance/failures.aarch64-darwin.nix
+++ maintenance/failures.aarch64-darwin.nix
@@ -1,11 +1,10 @@
 {
-  "appmenu-gtk3-module" = "/nix/store/zc2kk54s6xms1j7i8834wnaay1pscn53-appmenu-gtk3-module-0.7.6";
-  "discord-krisp" = "/nix/store/xg6pmxss890xcwaqg2g7qarn3d1mnz7x-discord-0.0.398";
-  "firedragon-bin" = "/nix/store/hm3gbmfhc6n1ck4z453g8ahy1bxy8pxg-firedragon-bin-12.9.1";
-  "firedragon-catppuccin-bin" = "/nix/store/c08gmd990xpgylwi7niyaaky09l1qg7x-firedragon-catppuccin-bin-12.9.1";
-  "helix_git" = "/nix/store/pidddyfxirmrf4kpqzaf2z0qy1si3qf9-helix-unwrapped-unstable-20260716163018-f6f3eb1";
-  "lan-mouse_git" = "/nix/store/2rmcwyc0wyb46ns7cclpi4f5wynf9wxn-lan-mouse-unstable-20260628142239-392af44";
-  "mesa_git" = "/nix/store/fyfy7f4pnfig8p8lmibr5mqgk4yvj9g4-mesa-99bf1b";
-  "vulkanPackages_latest.vulkan-tools" = "/nix/store/4gdn2ciykjv9pwfidynn30bydj3cw63h-vulkan-tools-1.4.356";
+  "appmenu-gtk3-module" = "/nix/store/n51c1mfl9xhxs3pa93vs4w5zfqhrdvcz-appmenu-gtk3-module-0.7.6";
+  "discord-krisp" = "/nix/store/mmwr7b6shksr32fhlscaq3l1ghj5202b-discord-0.0.398";
+  "firedragon-bin" = "/nix/store/w2zz3175qwl0cgb1jrgl65hzppbm8678-firedragon-bin-12.9.1";
+  "firedragon-catppuccin-bin" = "/nix/store/25am5m1nr5ahjrpwqw4am0infkcfashw-firedragon-catppuccin-bin-12.9.1";
+  "helix_git" = "/nix/store/i5jh37q4nfk23jw8gyyhi6vi3y0h482h-helix-unwrapped-unstable-20260723160337-079a789";
+  "mesa_git" = "/nix/store/dzcnzpcz0hz331fipy2ljx1xcbdzybwi-mesa-0d4d19";
+  "vulkanPackages_latest.vulkan-tools" = "/nix/store/7c5l3rqq9kmcw5p3wchf3y494rqf53y2-vulkan-tools-1.4.356";
   "zed-editor-fhs_git" = "skip";
 }
--- maintenance/failures.aarch64-linux.nix
+++ maintenance/failures.aarch64-linux.nix
@@ -1,4 +1,5 @@
 {
+  "niri_git" = "/nix/store/lrkhbmj2i47k4vdnzdkirqp61bmszsvi-niri-unstable-20260720180526-7f26c3e";
   "telegram-desktop-unwrapped_git" = "skip";
   "telegram-desktop_git" = "skip";
 }
