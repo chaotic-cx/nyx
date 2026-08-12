f1b54d9a44103644456dce542638d8a58e39752d
PedroHLC <root@pedrohlc.com>
2026-08-12 08:51:42 -0300

    niri_git: 20260720180526-7f26c3e -> 20260811204258-fec411e + fix

--- maintenance/failures.aarch64-linux.nix
+++ maintenance/failures.aarch64-linux.nix
@@ -1,5 +1,4 @@
 {
-  "niri_git" = "/nix/store/lrkhbmj2i47k4vdnzdkirqp61bmszsvi-niri-unstable-20260720180526-7f26c3e";
   "telegram-desktop-unwrapped_git" = "skip";
   "telegram-desktop_git" = "skip";
 }
