ec581c7bcef1b6d2986a43d7e5b93857a80ee2af
Niko Cantero <97130632+nyabinary@users.noreply.github.com>
2026-08-12 21:36:48 -0400

    firefox_nightly: 155.0a1-20260811085340-537539ad8 -> 155.0a1-20260812202037-6cee80f87

--- pkgs/firefox-nightly/default.nix
+++ pkgs/firefox-nightly/default.nix
@@ -1,23 +1,47 @@
 {
   lib,
-  current ? importJSON ./manifest.json,
   importJSON ? lib.trivial.importJSON,
+  current ? importJSON ./manifest.json,
   buildMozillaMach,
   callPackage,
+  fetchNpmDeps,
   fetchurl,
+  nodejs,
+  npmHooks,
   nss_git,
   nyxUtils,
+  python314,
   stdenv,
-  # Temporary fixes:
-  rust-cbindgen,
-  fetchFromGitHub,
-  rustPlatform,
+
+  # Platform-specific:
   apple-sdk_26,
+
+  # Temporary fixes:
+  fetchFromGitHub,
+  rust-cbindgen,
+  rustPlatform,
 }:
 
 let
   firefoxRepo = "mozilla-firefox/firefox";
   firefoxSourceRepo = "https://github.com/${firefoxRepo}";
+
+  binaryName = "firefox-nightly";
+  version = "${current.version}-${current.buildId}-${builtins.substring 0 7 current.rev}";
+
+  firefoxSrc = fetchurl {
+    inherit (current) hash;
+
+    url = "https://codeload.github.com/${firefoxRepo}/tar.gz/${current.rev}";
+    name = "firefox.tar.gz";
+  };
+
+  newtabNpmDeps = fetchNpmDeps {
+    src = firefoxSrc;
+    sourceRoot = "firefox-${current.rev}/browser/extensions/newtab";
+
+    hash = current.newtabNpmDepsHash;
+  };
 
   rust-cbindgen_latest =
     if lib.versionOlder rust-cbindgen.version "0.29.4" then
@@ -28,13 +52,14 @@ let
           src = fetchFromGitHub {
             owner = "mozilla";
             repo = "cbindgen";
-            tag = "${finalAttrs.version}";
+            tag = finalAttrs.version;
             hash = "sha256-leeHOwpzXuzg2cTjXehBnCsS+dvU4eIIFtWKeCee20U=";
           };
 
           cargoDeps = rustPlatform.fetchCargoVendor {
             inherit (finalAttrs) src;
             inherit (prevAttrs.cargoDeps) name;
+
             hash = "sha256-f6YoDoiVoh0BVPYHFO1FsdI4OCsF+LY72QaD57StdIQ=";
           };
         }
@@ -42,9 +67,8 @@ let
     else
       rust-cbindgen;
 
-  binaryName = "firefox-nightly";
-
-  updateScript = callPackage ./update.nix { };
+  updateScriptPackage = callPackage ./update.nix { };
+  updateScript = lib.getExe updateScriptPackage;
 
   removedPatches = [
     "133-env-var-for-system-dir.patch"
@@ -56,7 +80,6 @@ let
   addedPatches = [
     ./env_var_for_system_dir-ff-unstable.patch
     ./no-buildconfig-ffx-unstable.patch
-    ./relax-apple-sdk.patch
   ];
 
   isRustCbindgen =
@@ -71,28 +94,37 @@ let
 
   mach = buildMozillaMach {
     pname = "firefox-nightly";
-    inherit binaryName updateScript;
-    version = "${current.version}-${current.buildId}-${builtins.substring 0 7 current.rev}";
+
+    inherit
+      binaryName
+      updateScript
+      version
+      ;
+
     applicationName = "Firefox Nightly";
     requireSigning = false;
     branding = "browser/branding/nightly";
 
-    src = fetchurl {
-      inherit (current) hash;
-      url = "https://codeload.github.com/${firefoxRepo}/tar.gz/${current.rev}";
-      name = "firefox.tar.gz";
-    };
+    src = firefoxSrc;
 
     meta = {
       description = "Web browser built from Firefox Nightly source tree";
       homepage = "https://www.firefox.com/";
-      maintainers = with lib.maintainers; [ pedrohlc ];
+
+      maintainers = with lib.maintainers; [
+        pedrohlc
+      ];
+
       platforms = lib.platforms.unix;
       broken = stdenv.buildPlatform.is32bit;
       maxSilent = 14400;
+
       license = lib.licenses.mpl20;
       mainProgram = binaryName;
-      hydraPlatforms = [ "x86_64-linux" ];
+
+      hydraPlatforms = [
+        "x86_64-linux"
+      ];
     };
   };
 
@@ -105,13 +137,49 @@ let
       MOZ_INCLUDE_SOURCE_INFO = "1";
     };
 
-    nativeBuildInputs = map replaceRustCbindgen (prevAttrs.nativeBuildInputs or [ ]);
+    nativeBuildInputs = map replaceRustCbindgen (prevAttrs.nativeBuildInputs or [ ]) ++ [
+      nodejs
+    ];
 
     buildInputs =
       (prevAttrs.buildInputs or [ ]) ++ lib.optional stdenv.hostPlatform.isDarwin apple-sdk_26;
 
+    preBuild = (prevAttrs.preBuild or "") + ''
+      (
+        readonly newtab_root="''${MOZ_OBJDIR%/*}/browser/extensions/newtab"
+
+        export npmDeps=${newtabNpmDeps}
+        export npmRoot="$newtab_root"
+
+        source ${npmHooks.npmConfigHook}/nix-support/setup-hook
+        npmConfigHook
+
+        ${lib.getExe python314} -c ${lib.escapeShellArg ''
+          import hashlib
+          import sys
+          from pathlib import Path
+
+          newtab_root = Path(sys.argv[1])
+          lockfile = newtab_root / "package-lock.json"
+          stamp = newtab_root / "node_modules" / ".newtab-install-stamp"
+
+          with lockfile.open("rb") as lockfile_stream:
+              digest = hashlib.file_digest(lockfile_stream, "sha256").digest()
+
+          stamp.write_bytes(digest)
+        ''} "$newtab_root"
+
+        test -f "$newtab_root/node_modules/webpack/bin/webpack.js"
+      )
+    '';
+
     passthru = (prevAttrs.passthru or { }) // {
-      inherit updateScript;
+      inherit
+        newtabNpmDeps
+        updateScript
+        updateScriptPackage
+        ;
+
       rust-cbindgen = rust-cbindgen_latest;
     };
   };
