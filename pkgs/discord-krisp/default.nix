b4c631607b6ceb4cff56734136c192398bc834f9
PedroHLC <root@pedrohlc.com>
2026-08-13 08:17:01 -0300

    discord-krisp: adopt new FHS
    Introduced at https://github.com/NixOS/nixpkgs/pull/544831

--- pkgs/discord-krisp/default.nix
+++ pkgs/discord-krisp/default.nix
@@ -1,7 +1,7 @@
 { prev, ... }:
 let
   # krisp-patcher.py courtesy of https://github.com/sersorrel/sys
-  patch-krisp = prev.writers.writePython3 "krisp-patcher" {
+  krispPatcher = prev.writers.writePython3 "krisp-patcher" {
     libraries = with prev.python3Packages; [
       capstone
       pyelftools
@@ -14,16 +14,20 @@ let
     ];
   } (builtins.readFile ./krisp-patcher.py);
   binaryName = "Discord";
-  node_module = "\\$HOME/.config/discord/${prev.discord.version}/modules/discord_krisp/discord_krisp.node";
+  nodeModule = "\\$HOME/.config/discord/${prev.discord.version}/modules/discord_krisp/discord_krisp.node";
+
+  unwrappedPatched = prev.discord.unwrappedDiscord.overrideAttrs (previousAttrs: {
+    postInstall = previousAttrs.postInstall + ''
+      wrapProgramShell $out/opt/${binaryName}/${binaryName} \
+      --run "${krispPatcher} ${nodeModule}"
+    '';
+    passthru = removeAttrs previousAttrs.passthru [ "updateScript" ];
+    meta = {
+      nyx.bypassLicense = true;
+    }
+    // previousAttrs.meta;
+  });
 in
-prev.discord.overrideAttrs (previousAttrs: {
-  postInstall = previousAttrs.postInstall + ''
-    wrapProgramShell $out/opt/${binaryName}/${binaryName} \
-    --run "${patch-krisp} ${node_module}"
-  '';
-  passthru = removeAttrs previousAttrs.passthru [ "updateScript" ];
-  meta = {
-    nyx.bypassLicense = true;
-  }
-  // previousAttrs.meta;
-})
+prev.discord.override {
+  unwrappedDiscord = unwrappedPatched;
+}
