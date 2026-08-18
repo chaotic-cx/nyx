844e86b01206a9889b95d6903e58013497e60781
PedroHLC <root@pedrohlc.com>
2026-08-18 15:20:31 -0300

    failures: ignore more aarch64-darwin errors

--- maintenance/failures.aarch64-darwin.nix
+++ maintenance/failures.aarch64-darwin.nix
@@ -1,9 +1,9 @@
 {
-  "appmenu-gtk3-module" = "/nix/store/xj0vxsjxh9vzhararcv6kfrma30gl0an-appmenu-gtk3-module-0.7.6";
-  "discord-krisp" = "/nix/store/smwd4j2ynrs8ni978my2kfy1gcl1c2dp-discord-0.0.406";
-  "firedragon-bin" = "/nix/store/jvv1s2012yp84vnlw738sfdscahw2bm2-firedragon-bin-12.9.1";
-  "firedragon-catppuccin-bin" = "/nix/store/kyljxg9hjmzbdwpr68pw8a4wg0xy6a69-firedragon-catppuccin-bin-12.9.1";
-  "helix_git" = "/nix/store/scnn8m7dnq4wb7kxcr8rjl4l7dp1grwk-helix-unwrapped-unstable-20260723160337-079a789";
-  "vulkanPackages_latest.vulkan-tools" = "/nix/store/4g0al7k17im0llgbb687dhms7fc97236-vulkan-tools-1.4.356";
+  "appmenu-gtk3-module" = "skip";
+  "discord-krisp" = "skip";
+  "firedragon-bin" = "skip";
+  "firedragon-catppuccin-bin" = "skip";
+  "helix_git" = "skip";
+  "vulkanPackages_latest.vulkan-tools" = "skip";
   "zed-editor-fhs_git" = "skip";
 }
