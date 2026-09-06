1c0ba50bfd6f7b9a4cc55cbf0e76e663913eb3ce
Niko Cantero <97130632+nyabinary@users.noreply.github.com>
2026-09-06 19:21:26 -0400

    nss_git: fix; 20260826001434-1de6006 -> 20260903105110-7db8de4

--- pkgs/nss-git/default.nix
+++ pkgs/nss-git/default.nix
@@ -1,24 +1,30 @@
-{ prev, gitOverride, ... }:
+{
+  gitOverride,
+  nyxUtils,
+  prev,
+  ...
+}:
 
 gitOverride {
   nyxKey = "nss_git";
   prev = prev.nss_latest;
-
   manifestPath = "pkgs/nss-git/manifest.json";
   fetcher = "fetchFromGitHub";
   fetcherData = {
-    owner = "nss-dev";
+    owner = "mozilla";
     repo = "nss";
   };
   ref = "master";
 
   postOverride = prevAttrs: {
-    patches =
-      (builtins.filter (p: !prev.lib.hasSuffix "85_security_load_3.85+.patch" (toString p)) (
-        prevAttrs.patches or [ ]
-      ))
+    patches = nyxUtils.removeByBaseName "85_security_load_3.85+.patch" (prevAttrs.patches or [ ]) ++ [
+      # Fix build.sh with external NSPR by avoiding a lookup of a local nspr.pc
+      # that is not generated when NSS is built with --with-nspr.
+      ./fix-external-nspr-pkg-config.patch
+
       # Add NIX_NSS_LIBDIR fallback for loading libsoftokn3.so and related libs
       # when standard library paths are unavailable (e.g., in Nix isolated environments).
-      ++ [ ./nss-nix-path-PR.patch ];
+      ./nss-nix-path-PR.patch
+    ];
   };
 }
