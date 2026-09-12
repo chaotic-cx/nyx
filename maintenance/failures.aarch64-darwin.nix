45e36e6ce2a9757319f2e1a7bd9761490dbc24cc
PedroHLC <root@pedrohlc.com>
2026-09-11 23:49:05 -0300

    failures: update aarch64-{darwin,linux}

--- maintenance/failures.aarch64-darwin.nix
+++ maintenance/failures.aarch64-darwin.nix
@@ -1,7 +1,9 @@
 {
   "appmenu-gtk3-module" = "skip";
   "discord-krisp" = "skip";
+  "firedragon-bin-unwrapped" = "skip";
   "firedragon-bin" = "skip";
+  "firedragon-catppuccin-bin-unwrapped" = "skip";
   "firedragon-catppuccin-bin" = "skip";
   "helix_git" = "skip";
   "vulkanPackages_latest.vulkan-tools" = "skip";
--- maintenance/failures.aarch64-linux.nix
+++ maintenance/failures.aarch64-linux.nix
@@ -1,4 +1,8 @@
 {
+  "firedragon-bin-unwrapped" = "/nix/store/555r4bxyhkaa4mdw98ag9s9szn4d93ns-firedragon-bin-unwrapped-12.9.1";
+  "firedragon-bin" = "/nix/store/0sllnwxxg435flm236wrj1byrfa237xj-firedragon-bin-12.9.1";
+  "firedragon-catppuccin-bin-unwrapped" = "/nix/store/iznwafnhw296sb8xd8mfnh5saydjhgl1-firedragon-catppuccin-bin-unwrapped-12.9.1";
+  "firedragon-catppuccin-bin" = "/nix/store/6yspixry58f5j4358600xxywdjarwylz-firedragon-catppuccin-bin-12.9.1";
   "telegram-desktop-unwrapped_git" = "skip";
   "telegram-desktop_git" = "skip";
 }
